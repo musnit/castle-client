b0e335fe2d6d1e0a6d0dc66b7fc729309a4e81c2
Ben Roth <spongebucket@gmail.com>
2019-10-16 14:18:54 -0700

    WIP: obs framework

---
+++ macosx/obs/obs/obs.h
@@ -0,0 +1,19 @@
+//
+//  obs.h
+//  obs
+//
+//  Created by Ben Roth on 10/16/19.
+//  Copyright © 2019 Castle Games. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for obs.
+FOUNDATION_EXPORT double obsVersionNumber;
+
+//! Project version string for obs.
+FOUNDATION_EXPORT const unsigned char obsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <obs/PublicHeader.h>
+
+
