af5dcabce2ff563636ce7d011f185588011e567c
Jesse Ruder <jesse@getexponent.com>
2019-12-05 12:42:19 -0800

    try again

--- node/compile.js
+++ node/compile.js
@@ -1,22 +1,26 @@
 const { compile } = require('nexe');
 
 compile({
-  build: true,
-  make: ['-j4'],
-  input: './src/index.js',
-  targets: ['macos'],
-  output: 'daemon',
-  patches: [
-    /*(x, next) => {
-      x.code = () => [x.shims.join(''), x.input].join(';');
-      return next();
-    },*/
-    async (compiler, next) => {
-      //await compiler.setFileContentsAsync('lib/_third_party_main.js', compiler.code());
-      compiler.options.empty = true; // <-- ADDED THIS (hack)
-      return next();
-    },
-  ],
+  clean: true,
 }).then(() => {
-  console.log('success');
+  compile({
+    build: true,
+    make: ['-j4'],
+    input: './src/index.js',
+    targets: ['macos'],
+    output: 'castle-desktop-node-macos',
+    patches: [
+      (x, next) => {
+        x.code = () => [x.shims.join(''), x.input].join(';');
+        return next();
+      },
+      async (compiler, next) => {
+        await compiler.setFileContentsAsync('lib/_third_party_main.js', compiler.code());
+        compiler.options.empty = true; // <-- ADDED THIS (hack)
+        return next();
+      },
+    ],
+  }).then(() => {
+    console.log('success');
+  });
 });
