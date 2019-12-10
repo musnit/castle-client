bab40ed7621ec59349e901e668e2f1d28689dda3
Jesse Ruder <jesse@getexponent.com>
2019-12-10 10:31:42 -0800

    replace with empty node binary

--- node/compile.js
+++ node/compile.js
@@ -15,7 +15,9 @@ compile({
         return next();
       },
       async (compiler, next) => {
-        await compiler.setFileContentsAsync('lib/_third_party_main.js', compiler.code());
+        let code = compiler.code();
+        console.log(code);
+        await compiler.setFileContentsAsync('lib/_third_party_main.js', code);
         compiler.options.empty = true; // <-- ADDED THIS (hack)
         return next();
       },
