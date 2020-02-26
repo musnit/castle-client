e01144eb631d125eb1c9df3ffe814805024fcdca
Ben Roth <spongebucket@gmail.com>
2020-02-26 15:21:02 -0800

    mobile: new card transition with reanimated

--- mobile/js/CreateDeckNavigator.js
+++ mobile/js/CreateDeckNavigator.js
@@ -1,20 +1,32 @@
 import React from 'react';
+import { StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { Transitioning, Transition } from 'react-native-reanimated';
 
 import * as Constants from './Constants';
 
 import CreateDeckScreen from './CreateDeckScreen';
 import CreateCardScreen from './CreateCardScreen';
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+const transition = (
+  <Transition.Together>
+    <Transition.Out type="fade" durationMs={400} interpolation="easeIn" />
+    <Transition.Change interpolation="easeInOut" />
+    <Transition.Together>
+      <Transition.In type="scale" durationMs={400} interpolation="easeOut" />
+      <Transition.In type="fade" durationMs={200} delayMs={200} />
+    </Transition.Together>
+  </Transition.Together>
+);
+
 // switches between card creator and deck creator
-const CreateDeckNavigator = (props) => {
-  const navigation = useNavigation();
-  let deckId, cardId;
-  if (props.route && props.route.params) {
-    const { params } = props.route;
-    deckId = params.deckIdToEdit;
-    cardId = params.cardIdToEdit;
-  }
+const CreateDeckContent = ({ deckId, cardId, ...props }) => {
   if (deckId && !cardId) {
     return <CreateDeckScreen {...props} />;
   } else {
@@ -23,6 +35,32 @@ const CreateDeckNavigator = (props) => {
     }
     return <CreateCardScreen {...props} />;
   }
+};
+
+const CreateDeckNavigator = (props) => {
+  const navigation = useNavigation();
+  const transitionRef = React.useRef();
+  const [counter, setCounter] = React.useState(1);
+
+  let deckId, cardId;
+  if (props.route && props.route.params) {
+    const { params } = props.route;
+    deckId = params.deckIdToEdit;
+    cardId = params.cardIdToEdit;
+  }
+
+  React.useEffect(() => {
+    if (transitionRef.current) {
+      transitionRef.current.animateNextTransition();
+      setCounter(counter + 1);
+    }
+  }, [deckId, cardId]);
+
+  return (
+    <Transitioning.View ref={transitionRef} transition={transition} style={styles.container}>
+      <CreateDeckContent key={counter} deckId={deckId} cardId={cardId} {...props} />
+    </Transitioning.View>
+  );
 };
 
 export default CreateDeckNavigator;
