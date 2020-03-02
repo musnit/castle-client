e3d9d5b28827ef63f6df352a93a3ccc1a728155e
Ben Roth <spongebucket@gmail.com>
2020-03-02 15:57:33 -0800

    mobile: wip: fastlane beta testing

---
+++ mobile/tools/upload-beta-build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ ! -d "fastlane" ]
+then
+   echo "Can't find fastlane directory, make sure to run this script from 'mobile'"
+   exit 1
+fi
+
+# bundle update fastlane
+bundle exec fastlane beta
