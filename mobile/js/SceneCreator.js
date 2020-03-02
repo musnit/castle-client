c01f632ab6d407321b2ef9bf82c1e14237bc2c56
David Cole <dacole@gmail.com>
2020-03-02 11:35:04 -0800

    scene creator: use reanimated bottom sheet for inspector panel

--- mobile/js/SceneCreator.js
+++ mobile/js/SceneCreator.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome5'
 
+import BottomSheet from 'reanimated-bottom-sheet';
 import * as Constants from './Constants';
 
 const SceneCreatorAction = (props) => {
@@ -56,7 +57,29 @@ const SceneCreatorTool = (props) => {
     )
 }
 
-const SceneCreatorTools = () => {
+class SceneCreatorInspectorSheet extends React.Component {
+    _renderContent = () => (
+        <SceneCreatorInspectorPanel />
+    )
+  
+    _renderHeader = () => (
+        <SceneCreatorInspectorTools />
+    )
+  
+    render() {
+      return (
+        <React.Fragment>
+            <BottomSheet
+                snapPoints = {[300, 0]}
+                renderHeader = {this._renderHeader}
+                renderContent = {this._renderContent}
+            />
+        </React.Fragment>
+      )
+    }
+  }
+
+const SceneCreatorInspectorTools = () => {
     return (
         <View style={{
             flexDirection: 'row',
@@ -70,19 +93,18 @@ const SceneCreatorTools = () => {
     )
 }
 
-const SceneCreatorPanel = () => {
+const SceneCreatorInspectorPanel = () => {
     return (
         <View>
-            <SceneCreatorTools />
             <View style={{
                 backgroundColor: '#fff',
                 borderTopLeftRadius: 8,
                 borderTopRightRadius: 8,
                 padding: 16,
-                paddingBottom: 128,
+                paddingBottom: 500,
                 marginTop: 8,
             }}>
-                <Text style={{ color: '#888', letterSpacing: 0.5, textAlign: 'center' }}>BEHAVIORS</Text>
+                <Text style={{ color: '#888', letterSpacing: 0.5, textAlign: 'center' }}>INSPECTOR</Text>
             </View>
         </View>
     )
@@ -96,7 +118,11 @@ export const SceneCreator = () => {
             justifyContent: 'space-between',
         }}>
             <SceneCreatorActions />
-            <SceneCreatorPanel />
+            <View style={{ alignItems: 'center' }}>
+                <View style={{ ...Constants.styles.plainButton, ...Constants.styles.dropShadow }}><Text>Test</Text></View>
+            </View>
+            <View style={{ paddingBottom: 250, }}>{/* Hi I just exist to make the test button vertically centered */}</View>
+            <SceneCreatorInspectorSheet />
         </View>
     );
 };
