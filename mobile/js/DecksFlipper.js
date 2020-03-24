c44e5d6da79b159f95c4ef4489780390083278ec
Ben Roth <spongebucket@gmail.com>
2020-03-24 10:51:49 -0700

    mobile: working on animated deck flip behavior with pan and tap gestures

--- mobile/js/DecksFlipper.js
+++ mobile/js/DecksFlipper.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Animated, StyleSheet, Text, View } from 'react-native';
 import { useSafeArea, SafeAreaView } from 'react-native-safe-area-context';
+import { PanGestureHandler, TapGestureHandler, State } from 'react-native-gesture-handler';
 
 import Viewport from './viewport';
 
@@ -13,6 +14,14 @@ const DECK_FEED_ITEM_HEIGHT =
   (1 / Constants.CARD_RATIO) * 100 * vw + // height of card
   DECK_FEED_ITEM_MARGIN; // margin below cell
 
+const FLIP_MIN_TRANSLATE_Y = DECK_FEED_ITEM_HEIGHT * 0.4;
+const FLIP_MIN_VELOCITY_Y = 72;
+
+const SPRING_CONFIG = {
+  tension: 150,
+  friction: 50,
+};
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -23,7 +32,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginBottom: DECK_FEED_ITEM_MARGIN,
     borderRadius: 6,
-    position: 'absolute',
   },
   itemCard: {
     aspectRatio: Constants.CARD_RATIO,
@@ -36,28 +44,79 @@ const styles = StyleSheet.create({
 const DecksFlipper = () => {
   const insets = useSafeArea();
   const tabBarHeight = 49;
-  const centerContentY = (100 * vh - insets.top - DECK_FEED_ITEM_HEIGHT - tabBarHeight) * 0.4;
+  const centerContentY = Math.max(
+    40,
+    (100 * vh - insets.top - DECK_FEED_ITEM_HEIGHT - tabBarHeight) * 0.5
+  );
 
-  let contentY = new Animated.Value(centerContentY);
-  let prevContentY = Animated.add(contentY, -DECK_FEED_ITEM_HEIGHT);
-  let nextContentY = Animated.add(contentY, DECK_FEED_ITEM_HEIGHT);
+  let translateY = new Animated.Value(0);
+  let containerY = Animated.add(translateY, centerContentY - DECK_FEED_ITEM_HEIGHT);
+  const onPanGestureEvent = Animated.event([{ nativeEvent: { translationY: translateY } }]);
+
+  const snapTo = (toValue) => {
+    Animated.spring(translateY, { toValue, ...SPRING_CONFIG }).start(({ finished }) => {
+      translateY.setValue(0);
+    });
+  };
+
+  const snapToNext = () => snapTo(-DECK_FEED_ITEM_HEIGHT);
+  const snapToPrevious = () => snapTo(DECK_FEED_ITEM_HEIGHT);
+
+  const onPanStateChange = (event) => {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      const { translationY, velocityY } = event.nativeEvent;
+      if (translationY < -FLIP_MIN_TRANSLATE_Y || velocityY < -FLIP_MIN_VELOCITY_Y) {
+        snapToNext();
+      } else if (translationY > FLIP_MIN_TRANSLATE_Y || velocityY > FLIP_MIN_VELOCITY_Y) {
+        snapToPrevious();
+      } else {
+        snapTo(0);
+      }
+    }
+  };
+
+  const onTapPrevStateChange = (event) => {
+    if (event.nativeEvent.state === State.END) {
+      snapToPrevious();
+    }
+  };
+
+  const onTapNextStateChange = (event) => {
+    if (event.nativeEvent.state === State.END) {
+      snapToNext();
+    }
+  };
 
   return (
     <View style={styles.container}>
-      <Animated.View style={[styles.itemContainer, { backgroundColor: '#f00', top: prevContentY }]}>
-        <View style={styles.itemCard}>
-          <Text>Prev card</Text>
+      <Animated.View style={{ transform: [{ translateY: containerY }] }}>
+        <PanGestureHandler
+          onGestureEvent={onPanGestureEvent}
+          onHandlerStateChange={onPanStateChange}>
+          <TapGestureHandler onHandlerStateChange={onTapPrevStateChange}>
+            <Animated.View style={[styles.itemContainer, { backgroundColor: '#f00' }]}>
+              <View style={styles.itemCard}>
+                <Text>Prev card</Text>
+              </View>
+            </Animated.View>
+          </TapGestureHandler>
+        </PanGestureHandler>
+        <View style={[styles.itemContainer, { backgroundColor: '#0f0' }]}>
+          <View style={styles.itemCard}>
+            <Text>Content card</Text>
+          </View>
         </View>
-      </Animated.View>
-      <Animated.View style={[styles.itemContainer, { backgroundColor: '#0f0', top: contentY }]}>
-        <View style={styles.itemCard}>
-          <Text>Content card</Text>
-        </View>
-      </Animated.View>
-      <Animated.View style={[styles.itemContainer, { backgroundColor: '#00f', top: nextContentY }]}>
-        <View style={styles.itemCard}>
-          <Text>Next card</Text>
-        </View>
+        <PanGestureHandler
+          onGestureEvent={onPanGestureEvent}
+          onHandlerStateChange={onPanStateChange}>
+          <TapGestureHandler onHandlerStateChange={onTapNextStateChange}>
+            <Animated.View style={[styles.itemContainer, { backgroundColor: '#00f' }]}>
+              <View style={styles.itemCard}>
+                <Text>Next card</Text>
+              </View>
+            </Animated.View>
+          </TapGestureHandler>
+        </PanGestureHandler>
       </Animated.View>
     </View>
   );
