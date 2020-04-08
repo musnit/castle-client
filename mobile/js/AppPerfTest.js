fae25a8018fe390427cbed7e5de71de369ab7a5f
Ben Roth <spongebucket@gmail.com>
2020-04-08 11:34:23 -0700

    mobile: card cell perf test for android with dummy deck data

--- mobile/js/AppPerfTest.js
+++ mobile/js/AppPerfTest.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'transparent',
+  },
   cell: {
     backgroundColor: '#fff',
     height: 100,
@@ -10,17 +16,15 @@ const styles = StyleSheet.create({
   },
 });
 
+/**
+ * basic scrollview with nearly empty cells
+ */
+
 const items = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-const AppPerfTest = () => {
+const AppBasicScrollView = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#f00',
-      }}>
+    <View style={styles.container}>
       <ScrollView style={{ flex: 1, width: '100%' }} contentContainerStyle={{ flexGrow: 1 }}>
         {items.map((item, ii) => (
           <View key={ii} style={styles.cell}>
@@ -32,4 +36,37 @@ const AppPerfTest = () => {
   );
 };
 
-export default AppPerfTest;
+/**
+ * scrollview with cards
+ */
+import CardsSet from './CardsSet';
+import { DUMMY_DECK } from './DUMMY_DECK';
+
+const AppCardCellScrollView = () => {
+  /* const loadDeck = useQuery(
+    gql`
+      query Deck($deckId: ID!) {
+        deck(deckId: $deckId) {
+          ${DECK_FRAGMENT}
+        }
+      }
+    `,
+    { variables: { deckId: 1 }, fetchPolicy: 'no-cache' }
+  );
+
+  let deck;
+  if (!loadDeck.loading && !loadDeck.error && loadDeck.data) {
+    deck = loadDeck.data.deck;
+    } */
+  let deck = DUMMY_DECK;
+
+  return (
+    <View style={styles.container}>
+      <ScrollView style={{ flex: 1, width: '100%' }} contentContainerStyle={{ flexGrow: 1 }}>
+        <CardsSet showNewCard deck={deck} onPress={() => {}} />
+      </ScrollView>
+    </View>
+  );
+};
+
+export default AppCardCellScrollView;
---
+++ mobile/js/DUMMY_DECK.js
@@ -0,0 +1,185 @@
+// smallish image 420x550
+// https://d1vkcv80qw9qqp.cloudfront.net/470e4b763d5680b1af798719412a7a26
+// larger image 653x1024
+// https://d1vkcv80qw9qqp.cloudfront.net/cbbfcc15bfc9d97326170bae3ccf2b98
+
+export const DUMMY_DECK = {
+  id: '2',
+  deckId: '2',
+  title: 'The Mystery Deck',
+  isVisible: true,
+  cards: [
+    {
+      id: '2',
+      cardId: '2',
+      title: 'The Beginning',
+      updatedTime: '2020-03-13T23:18:57.554Z',
+      backgroundImage: {
+        url: 'https://d1vkcv80qw9qqp.cloudfront.net/470e4b763d5680b1af798719412a7a26',
+        primaryColor: '#2f2f36',
+      },
+    },
+    {
+      id: '10',
+      cardId: '10',
+      title: 'Because',
+      updatedTime: '2020-02-27T18:19:44.034Z',
+      backgroundImage: {
+        url: 'https://d1vkcv80qw9qqp.cloudfront.net/470e4b763d5680b1af798719412a7a26',
+        primaryColor: '#2f2f36',
+      },
+    },
+    {
+      id: '45',
+      cardId: '45',
+      title: 'In a computer',
+      updatedTime: '2020-02-27T18:19:53.734Z',
+      backgroundImage: {
+        url: 'https://d1vkcv80qw9qqp.cloudfront.net/470e4b763d5680b1af798719412a7a26',
+        primaryColor: '#2f2f36',
+      },
+    },
+    {
+      id: '49',
+      cardId: '49',
+      title: 'The end',
+      updatedTime: '2020-02-06T23:37:38.651Z',
+      backgroundImage: {
+        url: 'https://d1vkcv80qw9qqp.cloudfront.net/cbbfcc15bfc9d97326170bae3ccf2b98',
+        primaryColor: '#767a7c',
+      },
+    },
+    {
+      id: '46',
+      cardId: '46',
+      title: 'Salad',
+      updatedTime: '2020-03-06T22:08:45.871Z',
+      backgroundImage: {
+        url: 'https://d1vkcv80qw9qqp.cloudfront.net/470e4b763d5680b1af798719412a7a26',
+        primaryColor: '#2f2f36',
+      },
+    },
+    {
+      id: '11',
+      cardId: '11',
+      title: 'Don’t tell me',
+      updatedTime: '2020-02-27T18:17:50.665Z',
+      backgroundImage: {
+        url: 'https://d1vkcv80qw9qqp.cloudfront.net/470e4b763d5680b1af798719412a7a26',
+        primaryColor: '#2f2f36',
+      },
+    },
+    {
+      id: '48',
+      cardId: '48',
+      title: 'Tell me more',
+      updatedTime: '2020-02-25T10:35:18.085Z',
+      backgroundImage: {
+        url: 'https://d1vkcv80qw9qqp.cloudfront.net/470e4b763d5680b1af798719412a7a26',
+        primaryColor: '#2f2f36',
+      },
+    },
+    {
+      id: '47',
+      cardId: '47',
+      title: 'Excellent work',
+      updatedTime: '2020-02-27T18:23:21.595Z',
+      backgroundImage: {
+        url: 'https://d1vkcv80qw9qqp.cloudfront.net/470e4b763d5680b1af798719412a7a26',
+        primaryColor: '#2f2f36',
+      },
+    },
+    {
+      id: '4701',
+      cardId: '4701',
+      title: 'Excellent work',
+      updatedTime: '2020-02-27T18:23:21.595Z',
+      backgroundImage: {
+        url: 'https://d1vkcv80qw9qqp.cloudfront.net/470e4b763d5680b1af798719412a7a26',
+        primaryColor: '#2f2f36',
+      },
+    },
+    {
+      id: '4702',
+      cardId: '4702',
+      title: 'Excellent work',
+      updatedTime: '2020-02-27T18:23:21.595Z',
+      backgroundImage: {
+        url: 'https://d1vkcv80qw9qqp.cloudfront.net/470e4b763d5680b1af798719412a7a26',
+        primaryColor: '#2f2f36',
+      },
+    },
+    {
+      id: '4703',
+      cardId: '4703',
+      title: 'Excellent work',
+      updatedTime: '2020-02-27T18:23:21.595Z',
+      backgroundImage: {
+        url: 'https://d1vkcv80qw9qqp.cloudfront.net/470e4b763d5680b1af798719412a7a26',
+        primaryColor: '#2f2f36',
+      },
+    },
+    {
+      id: '4704',
+      cardId: '4704',
+      title: 'Excellent work',
+      updatedTime: '2020-02-27T18:23:21.595Z',
+      backgroundImage: {
+        url: 'https://d1vkcv80qw9qqp.cloudfront.net/470e4b763d5680b1af798719412a7a26',
+        primaryColor: '#2f2f36',
+      },
+    },
+  ],
+  initialCard: {
+    id: '2',
+    cardId: '2',
+  },
+};
+
+/* {
+      "id": "1",
+      "deckId": "1",
+      "title": "The First Deck EVer?",
+      "isVisible": false,
+      "cards": [
+        {
+          "id": "8",
+          "cardId": "8",
+          "title": "Can you even get to this card",
+          "updatedTime": "2020-02-11T23:57:20.134Z",
+          "backgroundImage": null
+        },
+        {
+          "id": "137",
+          "cardId": "137",
+          "title": "card-9",
+          "updatedTime": "2020-03-25T20:13:31.857Z",
+          "backgroundImage": {
+            "url": "https://d1vkcv80qw9qqp.cloudfront.net/0abf83d7c2e542237b86029ad2042724",
+            "primaryColor": "#cfbda2"
+          }
+        },
+        {
+          "id": "7",
+          "cardId": "7",
+          "title": "card-7",
+          "updatedTime": "2020-02-06T01:09:56.733Z",
+          "backgroundImage": null
+        },
+        {
+          "id": "1",
+          "cardId": "1",
+          "title": "Y helo thar",
+          "updatedTime": "2020-03-25T17:44:38.733Z",
+          "backgroundImage": {
+            "url": "https://d1vkcv80qw9qqp.cloudfront.net/7814438d4825c6e62f5e6ebb24487c04",
+            "primaryColor": "#9d1656"
+          }
+        }
+      ],
+      "initialCard": {
+        "id": "1",
+        "cardId": "1"
+      }
+}
+ */
