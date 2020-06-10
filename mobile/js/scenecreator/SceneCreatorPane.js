8e2e14ac4d30e075054eac60c6dca9dea586f145
David Cole <dacole@gmail.com>
2020-06-10 12:02:28 -0700

    creator: remove spacing from scene creator pane (spacing moved to lua)

--- mobile/js/scenecreator/SceneCreatorPane.js
+++ mobile/js/scenecreator/SceneCreatorPane.js
@@ -18,7 +18,7 @@ export default SceneCreatorPane = React.memo(
     const insets = useSafeArea();
 
     const renderContent = () => (
-      <ToolPane element={element} context={context} style={{ marginTop: 16, padding: 12, }} />
+      <ToolPane element={element} context={context} />
     );
 
     const middleSnapPoint = element.props?.contentHeight ?? SCREEN_HEIGHT * 0.4;
