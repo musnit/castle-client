16a7832c5b8da87d020e49a6b6ba8b40d67ac3b7
Ben Roth <spongebucket@gmail.com>
2020-06-12 14:39:04 -0700

    ios: android: js: add sentry

--- mobile/App.js
+++ mobile/App.js
@@ -3,4 +3,11 @@ require('node-libs-react-native/globals');
 
 import Main from './js/Main';
 
+import * as Sentry from '@sentry/react-native';
+
+Sentry.init({ 
+  dsn: 'https://0215bab0a1264d8db40e95388137688e@o406807.ingest.sentry.io/5275015', 
+});
+
+
 export default Main;
