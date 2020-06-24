c8b23aa70ffb0f72840840c643df156fb68590fb
David Cole <dacole@gmail.com>
2020-06-24 15:25:30 -0700

    inspector: move behavior styles to constants file

--- mobile/js/scenecreator/SceneCreatorConstants.js
+++ mobile/js/scenecreator/SceneCreatorConstants.js
@@ -1,3 +1,5 @@
+import * as Constants from '../Constants';
+
 export const Colors = {
   background: '#fff',
   text: '#222',
@@ -19,3 +21,18 @@ export const Colors = {
     shadow: '#0002',
   },
 };
+
+export const styles = {
+  behaviorContainer: {
+    marginHorizontal: 16,
+    marginVertical: 8,
+    borderWidth: 1,
+    borderBottomWidth: 2,
+    borderRadius: 6,
+    borderColor: Constants.colors.black,
+  },
+  behaviorProperties: {
+    padding: 12,
+    paddingBottom: 0,
+  }
+}
--- mobile/js/scenecreator/inspector/behaviors/InspectorGenericBehavior.js
+++ mobile/js/scenecreator/inspector/behaviors/InspectorGenericBehavior.js
@@ -1,32 +1,17 @@
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
 import { BehaviorPropertyInputRow } from '../components/BehaviorPropertyInputRow';
 import { BehaviorHeader } from '../components/BehaviorHeader';
 
-import * as Constants from '../../../Constants';
-
-const styles = StyleSheet.create({
-  container: {
-    marginHorizontal: 16,
-    marginVertical: 8,
-    borderWidth: 1,
-    borderBottomWidth: 2,
-    borderRadius: 6,
-    borderColor: Constants.colors.black,
-  },
-  properties: {
-    padding: 12,
-    paddingBottom: 0,
-  },
-});
+import * as SceneCreatorConstants from '../../SceneCreatorConstants';
 
 export default InspectorGenericBehavior = ({ behavior, sendAction, properties }) => {
   properties = properties ?? Object.keys(behavior.propertySpecs);
   return (
-    <View style={styles.container}>
+    <View style={SceneCreatorConstants.styles.behaviorContainer}>
       <BehaviorHeader name={behavior.displayName} onRemove={() => sendAction('remove')} />
       {properties?.length ? (
-        <View style={styles.properties}>
+        <View style={SceneCreatorConstants.styles.behaviorProperties}>
           {properties.map((propName, ii) => (
             <BehaviorPropertyInputRow
               key={`${behavior.name}-property-${ii}`}
