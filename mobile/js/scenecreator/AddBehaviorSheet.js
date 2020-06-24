943b26151817605666a44ff13d15b4bd6af7871b
Ben Roth <spongebucket@gmail.com>
2020-06-23 17:25:09 -0700

    inspector: add behavior sheet omits empty groups, shows empty state if there is nothing available to add

--- mobile/js/scenecreator/AddBehaviorSheet.js
+++ mobile/js/scenecreator/AddBehaviorSheet.js
@@ -6,6 +6,16 @@ import * as Inspector from './inspector/behaviors/InspectorBehaviors';
 
 const styles = StyleSheet.create({
   container: {},
+  emptyState: {
+    padding: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
+    minHeight: 128,
+  },
+  emptyStateLabel: {
+    fontSize: 16,
+    color: '#999',
+  },
   group: {
     padding: 16,
     borderBottomWidth: 1,
@@ -61,43 +71,67 @@ const AddBehavior = ({ behavior, onAdd, disabled }) => {
   );
 };
 
+const EmptyState = () => (
+  <View style={styles.emptyState}>
+    <Text style={styles.emptyStateLabel}>There are no behaviors left to add.</Text>
+  </View>
+);
+
 export default AddBehaviorSheet = ({ isOpen, onClose, context, behaviors, addBehavior }) => {
   const onPressAdd = (key) => {
     addBehavior(key);
     onClose();
   };
   const renderHeader = () => <BottomSheetHeader title="Add a behavior" onClose={onClose} />;
-  const renderContent = () => (
-    <View style={styles.container}>
-      {Inspector.MotionBehaviors.map((group, ii) => {
-        let isGroupEnabled = true;
-        if (group.dependencies.indexOf('Moving') !== -1 && !behaviors.Moving.isActive) {
-          // TODO: someday we may want a more general dependency check here
-          isGroupEnabled = false;
-        }
-        return (
-          <View key={`group-${ii}`} style={styles.group}>
-            <View style={styles.groupHeading}>
-              <Text style={[styles.groupLabel, !isGroupEnabled ? styles.groupLabelDisabled : null]}>
-                {group.label}
-              </Text>
-              {!isGroupEnabled ? (
-                <Text style={styles.groupDisabledExplanation}>&ndash; requires dynamic motion</Text>
-              ) : null}
-            </View>
-            {group.behaviors.map((key, jj) => (
-              <AddBehavior
-                key={`add-behavior-${key}`}
-                behavior={behaviors[key]}
-                onAdd={() => onPressAdd(key)}
-                disabled={!isGroupEnabled}
-              />
-            ))}
-          </View>
-        );
-      })}
-    </View>
-  );
+
+  let isSheetEmpty = true;
+  let isGroupVisible = {};
+  Inspector.MotionBehaviors.forEach((group) => {
+    isGroupVisible[group.label] = group.behaviors.some((behavior) => !behaviors[behavior].isActive);
+    if (isGroupVisible[group.label]) {
+      isSheetEmpty = false;
+    }
+  });
+
+  const renderContent = () =>
+    isSheetEmpty ? (
+      <EmptyState />
+    ) : (
+      <View style={styles.container}>
+        {Inspector.MotionBehaviors.filter((group) => isGroupVisible[group.label]).map(
+          (group, ii) => {
+            let isGroupEnabled = true;
+            if (group.dependencies.indexOf('Moving') !== -1 && !behaviors.Moving.isActive) {
+              // TODO: someday we may want a more general dependency check here
+              isGroupEnabled = false;
+            }
+            return (
+              <View key={`group-${ii}`} style={styles.group}>
+                <View style={styles.groupHeading}>
+                  <Text
+                    style={[styles.groupLabel, !isGroupEnabled ? styles.groupLabelDisabled : null]}>
+                    {group.label}
+                  </Text>
+                  {!isGroupEnabled ? (
+                    <Text style={styles.groupDisabledExplanation}>
+                      &ndash; requires dynamic motion
+                    </Text>
+                  ) : null}
+                </View>
+                {group.behaviors.map((key, jj) => (
+                  <AddBehavior
+                    key={`add-behavior-${key}`}
+                    behavior={behaviors[key]}
+                    onAdd={() => onPressAdd(key)}
+                    disabled={!isGroupEnabled}
+                  />
+                ))}
+              </View>
+            );
+          }
+        )}
+      </View>
+    );
 
   return (
     <CardCreatorBottomSheet
