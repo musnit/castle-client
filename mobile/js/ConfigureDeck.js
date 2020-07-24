b457280587805b5caa3246dd4e2642efd83c30cb
Ben Roth <spongebucket@gmail.com>
2020-07-24 12:47:55 -0700

    action sheet prompt to confirm before deleting deck

--- mobile/js/ConfigureDeck.js
+++ mobile/js/ConfigureDeck.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { TouchableOpacity, StyleSheet, Text, View } from 'react-native';
+import { useActionSheet } from '@expo/react-native-action-sheet';
 
 import ConfigureInput from './ConfigureInput';
 
@@ -21,16 +22,31 @@ const styles = StyleSheet.create({
   },
 });
 
-const ConfigureDeck = (props) => {
+const ConfigureDeck = ({ deck, onChange, onDeleteDeck }) => {
+  const { showActionSheetWithOptions } = useActionSheet();
+  const maybeDeleteDeck = React.useCallback(() => {
+    showActionSheetWithOptions(
+      {
+        title: 'Delete deck?',
+        options: ['Delete Deck', 'Cancel'],
+        destructiveButtonIndex: 0,
+      },
+      (index) => {
+        if (index === 0) {
+          onDeleteDeck();
+        }
+      }
+    );
+  }, [onDeleteDeck, showActionSheetWithOptions]);
   return (
     <View style={styles.container}>
       <ConfigureInput
         label="Name"
         placeholder="Choose a name for this deck"
-        value={props.deck.title}
-        onChangeText={(title) => props.onChange({ title })}
+        value={deck.title}
+        onChangeText={(title) => onChange({ title })}
       />
-      <TouchableOpacity style={styles.deleteButton} onPress={props.onDeleteDeck}>
+      <TouchableOpacity style={styles.deleteButton} onPress={maybeDeleteDeck}>
         <Text style={styles.deleteLabel}>Delete Deck</Text>
       </TouchableOpacity>
     </View>
