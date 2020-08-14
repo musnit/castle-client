18003bd153766f4860dd92447f6dda9a5a7c2c10
Ben Roth <spongebucket@gmail.com>
2020-08-14 13:24:13 -0700

    ios: revert changes to GhostView config

--- mobile/ios/GhostView.m
+++ mobile/ios/GhostView.m
@@ -6,8 +6,8 @@
 // GhostView
 //
 
-#define SCENE_CREATOR_USE_PROD_SCENE_CREATOR false
-#define SCENE_CREATOR_DEV_URI @"reload+http://192.168.7.83:8080/Client.lua"
+#define SCENE_CREATOR_USE_PROD_SCENE_CREATOR true
+#define SCENE_CREATOR_DEV_URI @"reload+http://192.168.1.146:8080/Client.lua"
 
 @implementation GhostView
 
