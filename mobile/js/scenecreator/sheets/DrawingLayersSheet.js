7b769b2d848d421af0b2069453c08d5479bcf594
Jesse Ruder <jesse@getexponent.com>
2020-12-29 16:39:43 -0800

    drawing layers add frame and cell menus

--- mobile/js/scenecreator/sheets/DrawingLayersSheet.js
+++ mobile/js/scenecreator/sheets/DrawingLayersSheet.js
@@ -1,6 +1,7 @@
 import React, { useCallback, useEffect, useMemo } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import { sendDataPaneAction, useFastDataMemo } from '../../ghost/GhostUI';
+import { useFastDataMemo } from '../../ghost/GhostUI';
+import { useActionSheet } from '@expo/react-native-action-sheet';
 import _ from 'lodash';
 
 import { BottomSheet } from '../../components/BottomSheet';
@@ -8,6 +9,7 @@ import { BottomSheet } from '../../components/BottomSheet';
 import FastImage from 'react-native-fast-image';
 import MCIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import DraggableFlatList from 'react-native-draggable-flatlist';
+import { ScrollView } from 'react-native-gesture-handler';
 
 const styles = StyleSheet.create({
   container: {
@@ -16,6 +18,7 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 6,
   },
   header: {
+    padding: 20,
     height: 64,
     flexDirection: 'row',
     borderBottomWidth: 1,
@@ -28,7 +31,6 @@ const styles = StyleSheet.create({
     height: 50,
   },
   headingLabel: {
-    paddingLeft: 20,
     color: '#000',
     fontWeight: '500',
     fontSize: 16,
@@ -38,8 +40,6 @@ const styles = StyleSheet.create({
   layerRow: {
     height: 64,
     flexDirection: 'row',
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
   },
   firstCell: {
     flexDirection: 'row',
@@ -48,6 +48,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     borderRightWidth: 1,
     borderRightColor: '#ccc',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
   },
   cell: {
     width: 64,
@@ -56,6 +58,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     borderRightWidth: 1,
     borderRightColor: '#ccc',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
   },
   selectedCell: {
     width: 64,
@@ -64,6 +68,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     borderWidth: 2,
     borderColor: '#000',
+  },
+  cellMenuButton: {
+    position: 'absolute',
+    backgroundColor: 'black',
+    bottom: 0,
+    right: 0,
+    borderTopLeftRadius: 5,
+    width: 35,
+    height: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
@@ -108,23 +123,42 @@ const renderItem = ({ item, index, drag, isActive }) => {
         </TouchableOpacity>
       </TouchableOpacity>
       {layer.frames.map((frame, idx) => {
-        return (
-          <TouchableOpacity
-            onPress={() =>
-              item.fastAction('onSelectLayerAndFrame', {
+        let isSelected = layer.selectedFrame == idx + 1 && layer.isSelected;
+        let isMenuAvailable = isSelected && idx > 0;
+        let onPress = isMenuAvailable
+          ? () =>
+              layer.showCellActionSheet({
+                isLinked: frame.isLinked,
                 layerId: layer.id,
                 frame: idx + 1,
               })
-            }
+          : () =>
+              item.fastAction('onSelectLayerAndFrame', {
+                frame,
+                layerId: layer.id,
+                frame: idx + 1,
+              });
+
+        return (
+          <TouchableOpacity
+            onPress={onPress}
             onLongPress={drag}
             key={idx}
-            style={
-              layer.selectedFrame == idx + 1 && layer.isSelected ? styles.selectedCell : styles.cell
-            }>
-            <FastImage
-              source={{ uri: `data:image/png;base64,${frame.base64Png}` }}
-              style={styles.image}
-            />
+            style={isSelected ? styles.selectedCell : styles.cell}>
+            {frame.isLinked ? (
+              <MCIcon name="link-variant" size={ICON_SIZE} color={'#000'} />
+            ) : (
+              <FastImage
+                source={{ uri: `data:image/png;base64,${frame.base64Png}` }}
+                style={styles.image}
+              />
+            )}
+
+            {isMenuAvailable && (
+              <View style={styles.cellMenuButton}>
+                <MCIcon name={'dots-horizontal'} size={ICON_SIZE} color={'#fff'} />
+              </View>
+            )}
           </TouchableOpacity>
         );
       })}
@@ -133,12 +167,41 @@ const renderItem = ({ item, index, drag, isActive }) => {
 };
 
 const DrawingLayers = useFastDataMemo('draw-layers', ({ fastData, fastAction }) => {
+  const { showActionSheetWithOptions } = useActionSheet();
   const [stateLayers, setStateLayers] = React.useState([]);
 
   // we need to pass stateSeletedLayerId into extraData instead of fastData.selectedLayerId because
   // otherwise extraData will trigger the FlatList update before stateLayers gets updated
   const [stateSeletedLayerId, setStateSelectedLayerId] = React.useState(null);
   const [stateSelectedFrame, setStateSelectedFrame] = React.useState(1);
+
+  const showCellActionSheet = useCallback((args) => {
+    let options = [
+      {
+        name: args.isLinked ? 'Unlink' : 'Link',
+        action: () => {
+          fastAction('onSetCellLinked', {
+            frame: args.frame,
+            layerId: args.layerId,
+            isLinked: !args.isLinked,
+          });
+        },
+      },
+    ];
+
+    showActionSheetWithOptions(
+      {
+        title: 'Cell Options',
+        options: options.map((option) => option.name).concat(['Cancel']),
+        cancelButtonIndex: options.length,
+      },
+      (buttonIndex) => {
+        if (buttonIndex < options.length) {
+          return options[buttonIndex].action();
+        }
+      }
+    );
+  }, []);
 
   useEffect(() => {
     if (fastData.layers) {
@@ -153,6 +216,7 @@ const DrawingLayers = useFastDataMemo('draw-layers', ({ fastData, fastAction }) 
         let layer = newLayers[i];
         layer.isSelected = layer.id === fastData.selectedLayerId;
         layer.fastAction = fastAction;
+        layer.showCellActionSheet = showCellActionSheet;
         layer.selectedFrame = fastData.selectedFrame;
 
         if (!Array.isArray(layer.frames)) {
@@ -174,45 +238,82 @@ const DrawingLayers = useFastDataMemo('draw-layers', ({ fastData, fastAction }) 
     setStateSelectedFrame(fastData.selectedFrame);
   }, [fastData.selectedFrame]);
 
-  const onAddLayer = useCallback(() => fastAction('onAddLayer'));
-  const onAddFrame = useCallback(() => fastAction('onAddFrame'));
+  const onAddLayer = useCallback(() => fastAction('onAddLayer'), []);
+  const onAddFrame = useCallback(() => fastAction('onAddFrame'), []);
   const keyExtractor = useCallback((item) => item.id, []);
   const onDragEnd = useCallback(({ data }) => {
     setStateLayers(data);
     fastAction('onReorderLayers', data.map((layer) => layer.id));
   }, []);
+  const showFrameActionSheet = useCallback((frame) => {
+    let options = [
+      {
+        name: 'Delete Frame',
+        action: () => {
+          fastAction('onDeleteFrame', frame);
+        },
+      },
+      {
+        name: 'Add Frame After',
+        action: () => {
+          fastAction('onAddFrameAtPosition', frame);
+        },
+      },
+    ];
+
+    showActionSheetWithOptions(
+      {
+        title: 'Frame Options',
+        options: options.map((option) => option.name).concat(['Cancel']),
+        cancelButtonIndex: options.length,
+      },
+      (buttonIndex) => {
+        if (buttonIndex < options.length) {
+          return options[buttonIndex].action();
+        }
+      }
+    );
+  }, []);
 
   return (
-    <View style={{ flex: 1 }}>
-      <View style={styles.layerRow}>
-        <TouchableOpacity onPress={onAddLayer} style={styles.firstCell}>
-          <Text>+ Add Layer</Text>
-        </TouchableOpacity>
-        {stateLayers.length > 0 &&
-          stateLayers[0].frames.map((frame, idx) => {
-            return (
-              <TouchableOpacity
-                onPress={() => fastAction('onSelectFrame', idx + 1)}
-                style={styles.cell}
-                key={idx}>
-                <Text style={stateSelectedFrame == idx + 1 && { fontWeight: 'bold' }}>
-                  {idx + 1}
-                </Text>
-              </TouchableOpacity>
-            );
-          })}
-        <TouchableOpacity onPress={onAddFrame} style={styles.cell}>
-          <Text>+</Text>
-        </TouchableOpacity>
+    <ScrollView horizontal>
+      <View style={{ flex: 1 }}>
+        <View style={styles.layerRow}>
+          <TouchableOpacity onPress={onAddLayer} style={styles.firstCell}>
+            <Text>+ Add Layer</Text>
+          </TouchableOpacity>
+          {stateLayers.length > 0 &&
+            stateLayers[0].frames.map((frame, idx) => {
+              let isSelected = stateSelectedFrame == idx + 1;
+              let onPress = isSelected
+                ? () => showFrameActionSheet(idx + 1)
+                : () => fastAction('onSelectFrame', idx + 1);
+
+              return (
+                <TouchableOpacity onPress={onPress} style={styles.cell} key={idx}>
+                  <Text style={isSelected && { fontWeight: 'bold' }}>{idx + 1}</Text>
+
+                  {isSelected && (
+                    <View style={styles.cellMenuButton}>
+                      <MCIcon name={'dots-horizontal'} size={ICON_SIZE} color={'#fff'} />
+                    </View>
+                  )}
+                </TouchableOpacity>
+              );
+            })}
+          <TouchableOpacity onPress={onAddFrame} style={styles.cell}>
+            <Text>+</Text>
+          </TouchableOpacity>
+        </View>
+        <DraggableFlatList
+          data={stateLayers}
+          renderItem={renderItem}
+          keyExtractor={keyExtractor}
+          onDragEnd={onDragEnd}
+          extraData={stateSeletedLayerId}
+        />
       </View>
-      <DraggableFlatList
-        data={stateLayers}
-        renderItem={renderItem}
-        keyExtractor={keyExtractor}
-        onDragEnd={onDragEnd}
-        extraData={stateSeletedLayerId}
-      />
-    </View>
+    </ScrollView>
   );
 });
 
@@ -221,6 +322,9 @@ export const DrawingLayersSheet = ({ onClose, ...props }) => {
     <View style={styles.header}>
       <View>
         <Text style={styles.headingLabel}>Layers</Text>
+      </View>
+      <View>
+        <MCIcon name="play" size={ICON_SIZE} color={'#000'} />
       </View>
     </View>
   );
