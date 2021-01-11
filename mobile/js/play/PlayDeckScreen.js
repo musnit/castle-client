84a53bcf57aedacfad1058abab6ec85fff650558
Ben Roth <ben@castle.xyz>
2021-01-11 15:41:13 -0800

    android: feed: back button closes current feed item

--- mobile/js/play/PlayDeckScreen.js
+++ mobile/js/play/PlayDeckScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StatusBar, StyleSheet, View } from 'react-native';
 import { useIsFocused, useFocusEffect } from '../ReactNavigation';
 
 import { DecksFeed } from '../components/DecksFeed';
