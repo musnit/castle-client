8728a91d79fb6101feb411921f11850eb4ef904d
Ben Roth <ben@castle.xyz>
2021-02-12 11:19:17 -0800

    tools: scene creator bundler can be either remote or local

--- mobile/tools/bundle-scene-creator.sh
+++ mobile/tools/bundle-scene-creator.sh
@@ -1,27 +1,47 @@
 #!/bin/sh
 # downloads scene_creator from the castle server and writes it to a particular destination path.
 # used by the iOS build to bundle scene creator for the app's first boot.
-# usage: bundle-scene-creator.sh <api version> <path to write>
 
 set -e
 
+if [ $# -ne 3 ]
+then
+    echo "Usages:"
+    echo "\t$(basename $0) --remote <api version> <path to write>"
+    echo "\t$(basename $0) --local <path to scene-creator> <path to write>"
+    exit 1
+fi
+
+DESTINATION_PATH=$3
+
 cd "$(dirname "$0")"
 
-API_VERSION=$1
-DESTINATION_PATH=$2
+if [ "$1" = "--remote" ]; then
+    API_VERSION=$2
 
-DOWNLOAD_URL='https://api.castle.xyz/api/scene-creator'
+    DOWNLOAD_URL='https://api.castle.xyz/api/scene-creator'
 
-TMP_DOWNLOAD_DIR=/tmp/castle-scene-creator-download
-mkdir -p $TMP_DOWNLOAD_DIR
-pushd $TMP_DOWNLOAD_DIR
+    TMP_DOWNLOAD_DIR=/tmp/castle-scene-creator-download
+    mkdir -p $TMP_DOWNLOAD_DIR
+    pushd $TMP_DOWNLOAD_DIR
 
-echo "Downloading scene creator version $API_VERSION..."
+    echo "Downloading scene creator version $API_VERSION..."
+    curl -L -o scene_creator "$DOWNLOAD_URL?apiVersion=$API_VERSION"
 
-curl -L -o scene_creator "$DOWNLOAD_URL?apiVersion=$API_VERSION"
+    echo "Writing zipped scene creator to $DESTINATION_PATH..."
+    mv scene_creator "$DESTINATION_PATH"
 
-echo "Writing zipped scene creator to $DESTINATION_PATH..."
+    popd
+fi
 
-mv scene_creator $DESTINATION_PATH
+if [ "$1" = "--local" ]; then
+    SCENE_CREATOR_PATH=$2
 
-popd
+    echo "Generating zip from local scene creator at $SCENE_CREATOR_PATH..."
+    pushd "$SCENE_CREATOR_PATH/scripts"
+    ./generate_zip.sh
+    popd
+
+    echo "Writing zipped scene creator to $DESTINATION_PATH..."
+    cp "$SCENE_CREATOR_PATH/scene_creator.love" "$DESTINATION_PATH"
+fi
