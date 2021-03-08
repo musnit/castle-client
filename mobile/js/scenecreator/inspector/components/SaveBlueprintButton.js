9c4b47b9531951c888be52cf9abe86b739c400a9
David Cole <dacole@gmail.com>
2021-03-08 15:24:33 -0800

    add missing file

---
+++ mobile/js/scenecreator/inspector/components/SaveBlueprintButton.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+import { sendDataPaneAction, useGhostUI } from '../../../ghost/GhostUI';
+
+import * as SceneCreatorConstants from '../../SceneCreatorConstants';
+
+export const SaveBlueprintButton = () => {
+  let data, sendAction;
+
+  const { root } = useGhostUI();
+  const element = root?.panes ? root.panes['sceneCreatorBlueprints'] : null;
+  if (element?.children.count) {
+    Object.entries(element.children).forEach(([key, child]) => {
+      if (child.type === 'data') {
+        data = child.props.data;
+        data.lastReportedEventId = child.lastReportedEventId;
+        sendAction = (action, value) => sendDataPaneAction(element, action, value, key);
+      }
+    });
+  }
+
+  return (
+    <TouchableOpacity
+      onPress={() => {
+        sendAction('updateBlueprint', data.saveBlueprintData);
+      }}
+      style={SceneCreatorConstants.styles.button}>
+      <Text style={SceneCreatorConstants.styles.buttonLabel}>Save blueprint</Text>
+    </TouchableOpacity>
+  );
+};
