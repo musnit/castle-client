e996c99522fa50e95a8b66b23c6ea453a63ef9bc
Ben Roth <ben@castle.xyz>
2021-03-09 13:21:23 -0800

    blueprint clipboard: use full cjson encoding of blueprint from lua

--- mobile/js/scenecreator/LibraryEntryClipboard.js
+++ mobile/js/scenecreator/LibraryEntryClipboard.js
@@ -2,15 +2,25 @@ import * as GhostEvents from '../ghost/GhostEvents';
 
 let gLibraryEntryClipboard;
 
+GhostEvents.listen('COPY_LIBRARY_ENTRY_DATA', (params) => {
+  try {
+    const entry = JSON.parse(params.data);
+    gLibraryEntryClipboard = entry;
+  } catch (e) {
+    console.warn(`Could not parse clipboard data from lua: ${e}`);
+  }
+});
+
 export const setLibraryEntryAsClipboard = (entry) => {
-  gLibraryEntryClipboard = JSON.parse(JSON.stringify(entry));
-  sync();
+  GhostEvents.sendAsync('COPY_LIBRARY_ENTRY', { entryId: entry.entryId });
+  // expect to assign clipboard in COPY_LIBRARY_ENTRY_DATA handler
 };
 
 export const sync = () => {
+  // tell lua which entry id already exists in the clipboard, if any
   const entry = gLibraryEntryClipboard;
   if (entry?.entryId) {
-    GhostEvents.sendAsync('COPY_LIBRARY_ENTRY', { entryId: entry.entryId });
+    GhostEvents.sendAsync('SYNC_COPIED_LIBRARY_ENTRY', { entryId: entry.entryId });
   }
 };
 
