ddec18573dc10b43ee841271fc4555bd23ef6e8d
Ben Roth <ben@castle.xyz>
2021-03-22 14:01:15 -0700

    create: add deckId to START_CREATING_NEW_DECK event

--- mobile/js/scenecreator/CreateCardScreenDataProvider.js
+++ mobile/js/scenecreator/CreateCardScreenDataProvider.js
@@ -101,7 +101,7 @@ class CreateCardScreenDataProvider extends React.Component {
         } catch (e) {
           throw new Error(`Unable to fetch logged in creator: ${e}`);
         }
-        Amplitude.logEvent('START_CREATING_NEW_DECK');
+        Amplitude.logEvent('START_CREATING_NEW_DECK', { deckId: deck.deckId });
       }
 
       let deckState;
