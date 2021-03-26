c64e78e9fe6e3e807c40a560a8195a01257e516c
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2021-03-25 19:39:35 -0700

    core: explain dummy drawing behavior

--- core/src/behaviors/dummy_drawing.h
+++ core/src/behaviors/dummy_drawing.h
@@ -9,6 +9,10 @@
 struct DummyDrawingComponent : BaseComponent {};
 
 class DummyDrawingBehavior : public BaseBehavior<DummyDrawingBehavior, DummyDrawingComponent> {
+  // Just draws the collision shapes of the Body behavior on the actor. Useful for debugging
+  // collision shapes and as a way to see actors for now until we get the real Drawing behavior to a
+  // useable state.
+
 public:
   static constexpr char name[] = "DummyDrawing";
 
