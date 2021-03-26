fa1b0d49855e95aac4fa62437d19681d70127e65
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2021-03-25 19:21:05 -0700

    core: make dummy drawing actually draw the fixture shape

--- core/src/behaviors/dummy_drawing.cpp
+++ core/src/behaviors/dummy_drawing.cpp
@@ -10,13 +10,31 @@
 void DummyDrawingBehavior::handleDrawComponent(
     ActorId actorId, const DummyDrawingComponent &component) const {
   if (auto maybeBodyComponent = getBehaviors().byType<BodyBehavior>().maybeGetComponent(actorId)) {
-    auto &bodyComponent = *maybeBodyComponent;
+    auto body = maybeBodyComponent->body;
+
     lv.graphics.push();
-    auto [x, y] = bodyComponent.body->GetPosition();
+    auto [x, y] = body->GetPosition();
     lv.graphics.translate(x, y);
-    lv.graphics.rotate(bodyComponent.body->GetAngle());
+    lv.graphics.rotate(body->GetAngle());
     lv.graphics.setColor(love::Colorf(0.4, 0.4, 0.2, 1));
-    lv.graphics.rectangle(love::Graphics::DRAW_FILL, -0.25, -0.25, 0.5, 0.5);
+
+    for (auto fixture = body->GetFixtureList(); fixture; fixture = fixture->GetNext()) {
+      auto shape = fixture->GetShape();
+      switch (shape->GetType()) {
+      case b2Shape::e_circle: {
+        auto circle = dynamic_cast<b2CircleShape *>(shape);
+        lv.graphics.circle(love::Graphics::DRAW_FILL, 0, 0, circle->m_radius);
+      } break;
+      case b2Shape::e_polygon: {
+        auto poly = dynamic_cast<b2PolygonShape *>(shape);
+        lv.graphics.polygon(
+            love::Graphics::DRAW_FILL, (love::Vector2 *)poly->m_vertices, poly->m_count, false);
+      } break;
+      default:
+        break;
+      }
+    }
+
     lv.graphics.pop();
   }
 }
