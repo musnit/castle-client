a0396905be4056f213063a50ac9b4528bc7d11ee
Ben Roth <ben@castle.xyz>
2021-04-08 15:12:47 -0700

    explore: stub out search bar

--- mobile/js/explore/ExploreScreen.js
+++ mobile/js/explore/ExploreScreen.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { StatusBar, StyleSheet, Linking, Text, TouchableOpacity, View } from 'react-native';
-
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { SearchInput } from './SearchInput';
 import { useFocusEffect, useNavigation } from '../ReactNavigation';
 
 import * as Amplitude from 'expo-analytics-amplitude';
@@ -12,6 +13,8 @@ const styles = StyleSheet.create({});
 export const ExploreScreen = ({ route }) => {
   useNavigation();
 
+  const [isSearching, setIsSearching] = React.useState(false);
+
   useFocusEffect(
     React.useCallback(() => {
       StatusBar.setBarStyle('light-content'); // needed for tab navigator
@@ -19,5 +22,13 @@ export const ExploreScreen = ({ route }) => {
     })
   );
 
-  return <SpringPartyCountdown />;
+  const onStartSearch = React.useCallback(() => setIsSearching(true), []);
+  const onCancelSearch = React.useCallback(() => setIsSearching(false), []);
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <SearchInput onFocus={onStartSearch} onCancel={onCancelSearch} />
+      {isSearching ? null : <SpringPartyCountdown />}
+    </SafeAreaView>
+  );
 };
---
+++ mobile/js/explore/SearchInput.js
@@ -0,0 +1,86 @@
+import * as React from 'react';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+
+import Feather from 'react-native-vector-icons/Feather';
+
+import * as Constants from '../Constants';
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 16,
+    borderWidth: 1,
+    borderRadius: 4,
+    borderColor: Constants.colors.white,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  searchIcon: {
+    paddingLeft: 4,
+  },
+  input: {
+    color: Constants.colors.white,
+    padding: 8,
+    width: '100%',
+    flexShrink: 1,
+  },
+  cancelButton: {
+    flexShrink: 0,
+    paddingRight: 8,
+  },
+  cancel: {
+    color: Constants.colors.white,
+  },
+});
+
+// TODO: actually search something
+export const SearchInput = ({ onFocus, onCancel, ...props }) => {
+  const [isFocused, setIsFocused] = React.useState(false);
+  const textInputRef = React.useRef(null);
+
+  const onTextInputFocus = React.useCallback(
+    (e) => {
+      setIsFocused(true);
+      if (onFocus) {
+        onFocus(e);
+      }
+    },
+    [onFocus]
+  );
+  const onTextInputBlur = React.useCallback(
+    (e) => {
+      setIsFocused(false);
+      if (onCancel) {
+        onCancel();
+      }
+    },
+    [onCancel]
+  );
+  const cancelSearch = React.useCallback(() => {
+    if (textInputRef.current) {
+      textInputRef.current.blur();
+    }
+    if (onCancel) {
+      onCancel();
+    }
+  }, [onCancel]);
+
+  return (
+    <View style={styles.container}>
+      <Feather name="search" color={Constants.colors.white} size={20} style={styles.searchIcon} />
+      <TextInput
+        ref={textInputRef}
+        style={styles.input}
+        placeholder="Search for people..."
+        placeholderTextColor={Constants.colors.white}
+        onFocus={onTextInputFocus}
+        onBlur={onTextInputBlur}
+        {...props}
+      />
+      {isFocused ? (
+        <TouchableOpacity style={styles.cancelButton} onPress={cancelSearch}>
+          <Text style={styles.cancel}>Cancel</Text>
+        </TouchableOpacity>
+      ) : null}
+    </View>
+  );
+};
