12cde2e0dead47d179286222139b7fd0923760f1
Ben Roth <ben@castle.xyz>
2021-04-12 14:09:32 -0700

    lua bridge: remove dead code

--- mobile/js/game/LuaBridge.js
+++ mobile/js/game/LuaBridge.js
@@ -8,62 +8,6 @@ import { sendAsync, useListen } from '../ghost/GhostEvents';
 import * as Session from '../Session';
 
 ///
-/// JS -> Lua for GraphQL entities
-///
-
-export const LUA_USER_FRAGMENT = gql`
-  fragment LuaUser on User {
-    id
-    userId
-    username
-    name
-    photo {
-      fileId
-      url
-    }
-  }
-`;
-
-export const jsUserToLuaUser = async (user) =>
-  user
-    ? {
-        userId: user.userId,
-        username: user.username,
-        name: user.name,
-        photoUrl: user.photo ? user.photo.url : undefined,
-      }
-    : undefined;
-
-export const jsPostToLuaPost = async ({ postId, creator, media }, { data }) => ({
-  postId,
-  creator: await jsUserToLuaUser(creator),
-  mediaUrl: media ? media.url : undefined,
-  data: data ? await Actions.postDataAsync({ postId }) : undefined,
-});
-
-export const LUA_GAME_FRAGMENT = gql`
-  fragment LuaGame on Game {
-    id
-    gameId
-    owner {
-      ...LuaUser
-    }
-    title
-    url
-    description
-  }
-  ${LUA_USER_FRAGMENT}
-`;
-
-export const jsGameToLuaGame = async ({ gameId, owner, title, url, description }) => ({
-  gameId,
-  owner: await jsUserToLuaUser(owner),
-  title,
-  url,
-  description,
-});
-
-///
 /// Lua -> JS -> Lua calls
 ///
 
