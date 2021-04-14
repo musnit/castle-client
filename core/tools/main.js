f6553e209aa0c466d0c780872a3b91f65be62b2f
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2021-04-13 18:01:05 -0700

    core: make load card script support deck ids and out file path

--- core/tools/main.js
+++ core/tools/main.js
@@ -1,20 +1,38 @@
 #!/usr/bin/env node
 
-const Axios = require("axios");
-const fs = require("fs");
-const path = require("path");
+const Axios = require('axios');
+const fs = require('fs');
+const path = require('path');
 
 async function runAsync() {
-  const response = await Axios.post("https://api.castle.xyz/graphql", {
-    operationName: null,
-    variables: {},
-    query: `{\n  card(cardId: "${process.argv[2]}") {\n    sceneData\n  }\n}\n`,
-  });
+  const outPath = process.argv[3] || '../assets/test-watch.json';
+  let snapshot;
 
-  fs.writeFileSync(
-    path.join(__dirname, "../assets/test-watch.json"),
-    JSON.stringify(response.data.data.card.sceneData.snapshot, null, 2)
-  );
+  try {
+    // Try as card
+    const response = await Axios.post('https://api.castle.xyz/graphql', {
+      operationName: null,
+      variables: {},
+      query: `{\n  card(cardId: "${process.argv[2]}") {\n    sceneData\n  }\n}\n`,
+    });
+    snapshot = response.data.data.card.sceneData.snapshot;
+  } catch (e) {
+    // Try as deck
+    try {
+      const response = await Axios.post('https://api.castle.xyz/graphql', {
+        operationName: null,
+        variables: {},
+        query: `{\n  deck(deckId: "${process.argv[2]}") {\n    initialCard { sceneData }\n  }\n}\n`,
+      });
+      snapshot = response.data.data.deck.initialCard.sceneData.snapshot;
+    } catch (e) {}
+  }
+
+  if (snapshot) {
+    fs.writeFileSync(path.join(__dirname, outPath), JSON.stringify(snapshot, null, 2));
+  } else {
+    console.log("Couldn't find any card or deck with this id...");
+  }
 }
 
 runAsync();
