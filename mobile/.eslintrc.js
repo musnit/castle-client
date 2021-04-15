f4450e29d342738315f7b999acc0da2d961bac1a
Ben Roth <ben@castle.xyz>
2021-04-15 11:30:26 -0700

    eslint: downgrade react-hooks to warn, other updates

--- mobile/.eslintrc.js
+++ mobile/.eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    "curly": 0,
+    "quotes": 0, // let prettier manage
+    "no-shadow": "off",
+    "react-hooks/rules-of-hooks": "warn",
+    "react-hooks/exhaustive-deps": "warn",
+    "react-native/no-inline-styles": 0,
+  },
 };
