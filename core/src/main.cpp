74b47c114c62b6d9b7fca16ff5d6e7e1591e73ec
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2021-04-21 13:33:59 -0700

    core: support 'dev_settings.h' for local developer settings

--- core/src/main.cpp
+++ core/src/main.cpp
@@ -3,9 +3,17 @@
 #include "platform.h"
 
 
-// Run the main loop, calling `frame` per frame. `frame` should return a
-// boolean that is `false` when it wants to quit. Handles both the web and
-// desktop cases.
+// Developer settings. You can add an 'src/dev_settings.h' file with your own local overrides for
+// these values -- that file is '.gitignore'd and will not affect upstream.
+#if __has_include("dev_settings.h")
+#include "dev_settings.h"
+#else
+#define DEV_DEFAULT_SCENE_FILENAME "test-watch.json"
+#endif
+
+
+// Run the main loop, calling `frame` per frame. `frame` should return a boolean that is `false`
+// when it wants to quit. Handles both the web and desktop cases.
 template<typename F>
 void loop(F &&frame) {
 #ifdef __EMSCRIPTEN__
@@ -26,7 +34,7 @@ void loop(F &&frame) {
 int main(int argc, char *argv[]) {
   Engine eng;
 
-  auto scenePath = Platform::getAssetPath(argc > 1 ? argv[1] : "test-rules-1.json");
+  auto scenePath = Platform::getAssetPath(argc > 1 ? argv[1] : DEV_DEFAULT_SCENE_FILENAME);
   if (!eng.hasInitialDeck()) {
     eng.loadSceneFromFile(scenePath.c_str());
   }
