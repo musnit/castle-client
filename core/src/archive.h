22926d3cb7b96216c3104b0f35e08d7f83b9121f
Ben Roth <ben@castle.xyz>
2021-07-16 16:02:06 -0700

    WIP: use behavior getter to serialize components

--- core/src/archive.h
+++ core/src/archive.h
@@ -243,6 +243,8 @@ public:
   void setNum(double val);
   template<typename V>
   void setStr(V &&val);
+  template<typename K, typename V>
+  void setMember(K &&key, V &&val);
 
 
   // Write a value of custom type. Can be a function, a type with a `T::write(Writer &writer)`
@@ -804,6 +806,11 @@ void Writer::str(V &&val) {
 template<typename V>
 void Writer::setStr(V &&val) {
   *cur = makeStr(std::forward<V>(val));
+}
+
+template<typename K, typename V>
+void Writer::setMember(K &&key, V &&val) {
+  cur->AddMember(makeStr(std::forward<K>(key)), write_(val), alloc);
 }
 
 template<typename K, typename F>
--- core/src/editor/editor.cpp
+++ core/src/editor/editor.cpp
@@ -335,11 +335,36 @@ void Editor::sendAllBehaviorsData() {
   bridge.sendEvent("EDITOR_ALL_BEHAVIORS", ev);
 };
 
-template<typename C>
+template<typename Behavior, typename Component>
 struct EditorSelectedComponentEvent {
-  PROP(bool, isDisabled) = false;
-  PROP(typename C::Props *, props);
+  ActorId actorId;
+  Component *component;
+
+  void write(Writer &writer) const;
 };
+
+template<typename Behavior, typename Component>
+void EditorSelectedComponentEvent<Behavior, Component>::write(Writer &writer) const {
+  if (component) {
+    auto &behavior = writer.getScene()->getBehaviors().byType<Behavior>();
+    writer.obj([&]() {
+      Props::forEach(component->props, [&](auto &prop) {
+        if constexpr (!Archive::skipProp<std::remove_reference_t<decltype(prop())>>) {
+          using Prop = std::remove_reference_t<decltype(prop)>;
+          using PropValue = std::remove_reference_t<decltype(prop())>;
+          constexpr auto propName = Prop::name;
+          if constexpr (std::is_arithmetic_v<PropValue>) {
+            // use behavior's getter when possible
+            writer.setMember(
+                std::string(propName), behavior.handleGetProperty(actorId, *component, prop.id));
+          } else {
+            writer.setMember(std::string(propName), prop());
+          }
+        }
+      });
+    });
+  }
+}
 
 struct EditorSelectedRulesComponentEvent {
   PROP(bool, isDisabled) = false;
@@ -353,8 +378,9 @@ struct EditorNoComponentEvent {
 // send behavior property values for the selected actor's components
 void Editor::sendSelectedComponent(int behaviorId) {
   scene->getBehaviors().byId(behaviorId, [&](auto &behavior) {
+    auto actorId = selection.firstSelectedActorId();
     using BehaviorType = std::remove_reference_t<decltype(behavior)>;
-    auto component = behavior.maybeGetComponent(selection.firstSelectedActorId());
+    auto component = behavior.maybeGetComponent(actorId);
     std::string eventName = std::string("EDITOR_SELECTED_COMPONENT:") + BehaviorType::name;
     if (component) {
       if constexpr (std::is_same_v<RulesBehavior, BehaviorType>) {
@@ -366,7 +392,7 @@ void Editor::sendSelectedComponent(int behaviorId) {
         bridge.sendEvent("EDITOR_SELECTED_COMPONENT:Rules", ev);
       } else {
         using ComponentType = std::remove_reference_t<decltype(*component)>;
-        EditorSelectedComponentEvent<ComponentType> ev { component->disabled, &component->props };
+        EditorSelectedComponentEvent<BehaviorType, ComponentType> ev { actorId, component };
         bridge.sendEvent(eventName.c_str(), ev);
       }
     } else {
