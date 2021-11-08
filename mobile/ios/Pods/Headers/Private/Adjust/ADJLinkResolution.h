252192ee7f9065bb04cb98b8fdd00528fff50ce5
Ben Roth <ben@castle.xyz>
2021-11-08 15:55:39 -0800

    add react-native-adjust

---
+++ mobile/ios/Pods/Headers/Private/Adjust/ADJLinkResolution.h
@@ -0,0 +1,1 @@
+../../../Adjust/Adjust/ADJLinkResolution.h
---
+++ mobile/ios/Pods/Target Support Files/react-native-adjust/react-native-adjust-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_react_native_adjust : NSObject
+@end
+@implementation PodsDummy_react_native_adjust
+@end
