5ee38491b237109a8d9fa68ef301a0ca6cfc9039
Nikhilesh S <s.nikhilesh@gmail.com>
2021-11-11 12:24:30 -0800

    core: allow picking between multiple fonts

--- core/tools/gen_font_data.sh
+++ core/tools/gen_font_data.sh
@@ -3,7 +3,8 @@
 # Converts '*.ttf' under './tools' into header files under './src/data'
 
 pushd ./tools > /dev/null
+rm -f ../src/data/fonts.h
 for f in *.ttf; do
-  xxd -i $f > ../src/data/$f.h
+  xxd -i $f >> ../src/data/fonts.h
 done
 popd > /dev/null
