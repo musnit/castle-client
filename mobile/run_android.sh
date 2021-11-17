d55dc9fa4aceba7afefd14f5228f465c20e11d9c
Jesse Ruder <jesseruder@gmail.com>
2021-11-17 13:30:59 -0800

    android: update run_android to exit if no device connected

--- mobile/run_android.sh
+++ mobile/run_android.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
+if [ -n "$ONLY_DEVICE" ]; then
+  if adb devices | grep -q '	device'; then
+    echo "Found connected device"
+  else
+    echo "No connected device"
+    exit 1
+  fi
+fi
+
 npx react-native run-android --appIdSuffix "debug"
