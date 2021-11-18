bf78d41287ab0275a61fd4118145f1b8b426a6aa
Nikhilesh S <s.nikhilesh@gmail.com>
2021-11-18 15:09:45 -0800

    core: bring in 28 test fonts

--- core/src/behaviors/text.h
+++ core/src/behaviors/text.h
@@ -24,7 +24,36 @@ struct TextComponent : BaseComponent {
          .label("Font")
          .rulesGet(false)
          .rulesSet(false)
-         .allowedValues("Comic Sans", "Roboto")
+         .allowedValues(
+            "3270Condensed Condensed",
+            "Abibas Medium",
+            "AstralMono Regular",
+            "Avara Bold",
+            "Avara BoldItalic",
+            "Betatron Regular",
+            "Blocus Regular",
+            "BreiteGrotesk Regular",
+            "Chicagoland Medium",
+            "ComicNeue Bold",
+            "ComicNeueAngular Bold",
+            "Compagnon Bold",
+            "Compagnon Medium",
+            "Compagnon Roman",
+            "DagsenOutline Black",
+            "Glacier Bold",
+            "HappyTimesAtTheIKOB Regular",
+            "HelicoCentrica Roman",
+            "Norm Medium",
+            "Norm Regular",
+            "Outward Block",
+            "Piazzolla Medium",
+            "SnapitMono Regular",
+            "SpaceGrotesk Regular",
+            "StandardGraf Regular",
+            "Syne Extra",
+            "YatraOne Regular",
+            "Zarathustra Regular"
+           )
          )
             = "Roboto";
   } props;
