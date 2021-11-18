7c48dac779bc46d64ac6618764ce90555d18e271
Nikhilesh S <s.nikhilesh@gmail.com>
2021-11-18 15:09:45 -0800

    core: bring in 28 test fonts

--- core/src/behaviors/text.cpp
+++ core/src/behaviors/text.cpp
@@ -159,8 +159,34 @@ TextBehavior::TextBehavior(Scene &scene_)
     fonts[name] = std::unique_ptr<love::Font>(lv.graphics.newFont(rasterizer));
   };
 
-  loadFont("Comic Sans", comic_ttf);
-  loadFont("Roboto", roboto_ttf);
+  loadFont("3270Condensed Condensed", __3270Condensed_Condensed_ttf);
+  loadFont("Abibas Medium", Abibas_Medium_ttf);
+  loadFont("AstralMono Regular", AstralMono_Regular_ttf);
+  loadFont("Avara Bold", Avara_Bold_ttf);
+  loadFont("Avara BoldItalic", Avara_BoldItalic_ttf);
+  loadFont("Betatron Regular", Betatron_Regular_ttf);
+  loadFont("Blocus Regular", Blocus_Regular_ttf);
+  loadFont("BreiteGrotesk Regular", BreiteGrotesk_Regular_ttf);
+  loadFont("Chicagoland Medium", Chicagoland_Medium_ttf);
+  loadFont("ComicNeue Bold", ComicNeue_Bold_ttf);
+  loadFont("ComicNeueAngular Bold", ComicNeueAngular_Bold_ttf);
+  loadFont("Compagnon Bold", Compagnon_Bold_ttf);
+  loadFont("Compagnon Medium", Compagnon_Medium_ttf);
+  loadFont("Compagnon Roman", Compagnon_Roman_ttf);
+  loadFont("DagsenOutline Black", DagsenOutline_Black_ttf);
+  loadFont("Glacier Bold", Glacier_Bold_ttf);
+  loadFont("HappyTimesAtTheIKOB Regular", HappyTimesAtTheIKOB_Regular_ttf);
+  loadFont("HelicoCentrica Roman", HelicoCentrica_Roman_ttf);
+  loadFont("Norm Medium", Norm_Medium_ttf);
+  loadFont("Norm Regular", Norm_Regular_ttf);
+  loadFont("Outward Block", Outward_Block_ttf);
+  loadFont("Piazzolla Medium", Piazzolla_Medium_ttf);
+  loadFont("SnapitMono Regular", SnapitMono_Regular_ttf);
+  loadFont("SpaceGrotesk Regular", SpaceGrotesk_Regular_ttf);
+  loadFont("StandardGraf Regular", StandardGraf_Regular_ttf);
+  loadFont("Syne Extra", Syne_Extra_ttf);
+  loadFont("YatraOne Regular", YatraOne_Regular_ttf);
+  loadFont("Zarathustra Regular", Zarathustra_Regular_ttf);
 }
 
 
