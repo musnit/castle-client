ee3d894ecb7fce0845df78d53424dc30a08d35a9
Nikhilesh S <s.nikhilesh@gmail.com>
2021-12-08 19:06:23 -0800

    core: emoji fonts,store font data as actual asset files (ios)

--- core/src/behaviors/text_fonts.cpp
+++ core/src/behaviors/text_fonts.cpp
@@ -1,7 +1,6 @@
 #include "text.h"
 
 #include "behaviors/all.h"
-#include "data/fonts.h"
 
 
 struct EmbeddedFontData : love::Data {
@@ -32,44 +31,60 @@ struct EmbeddedFontData : love::Data {
   }
 };
 
+namespace CastleCore {
+const char *getAssetsDirectoryPath();
+}
+
+#ifndef LOVE_IOS
+namespace CastleCore {
+const char *getAssetsDirectoryPath() {
+  return "assets/";
+}
+}
+#endif
+
 void TextBehavior::loadFontResources() {
   constexpr auto defaultFontSize = 10;
 
   defaultFont.reset(
       lv.graphics.newDefaultFont(defaultFontSize, love::TrueTypeRasterizer::HINTING_NORMAL));
 
-  const auto loadFontResource = [&](const std::string &name, auto &xxdData) {
+  const auto loadFontResource = [&](const std::string &name) {
     TextFontResource resource;
-    resource.data = love::StrongRef<love::Data>(new EmbeddedFontData(xxdData), love::Acquire::NORETAIN);
+    auto fullPath = CastleCore::getAssetsDirectoryPath() + std::string("/") + name + ".ttf";
+    resource.data = love::StrongRef<love::Data>(
+        lv.filesystem.read(fullPath.c_str()), love::Acquire::NORETAIN);
     fontResources[name] = std::move(resource);
   };
 
-  loadFontResource("3270Condensed Condensed", __3270Condensed_Condensed_ttf);
-  loadFontResource("Abibas Medium", Abibas_Medium_ttf);
-  loadFontResource("AstralMono Regular", AstralMono_Regular_ttf);
-  loadFontResource("Avara Bold", Avara_Bold_ttf);
-  loadFontResource("Avara BoldItalic", Avara_BoldItalic_ttf);
-  loadFontResource("Betatron Regular", Betatron_Regular_ttf);
-  loadFontResource("Blocus Regular", Blocus_Regular_ttf);
-  loadFontResource("BreiteGrotesk Regular", BreiteGrotesk_Regular_ttf);
-  loadFontResource("Chicagoland Medium", Chicagoland_Medium_ttf);
-  loadFontResource("ComicNeue Bold", ComicNeue_Bold_ttf);
-  loadFontResource("ComicNeueAngular Bold", ComicNeueAngular_Bold_ttf);
-  loadFontResource("Compagnon Bold", Compagnon_Bold_ttf);
-  loadFontResource("Compagnon Medium", Compagnon_Medium_ttf);
-  loadFontResource("Compagnon Roman", Compagnon_Roman_ttf);
-  loadFontResource("DagsenOutline Black", DagsenOutline_Black_ttf);
-  loadFontResource("Glacier Bold", Glacier_Bold_ttf);
-  loadFontResource("HappyTimesAtTheIKOB Regular", HappyTimesAtTheIKOB_Regular_ttf);
-  loadFontResource("HelicoCentrica Roman", HelicoCentrica_Roman_ttf);
-  loadFontResource("Norm Medium", Norm_Medium_ttf);
-  loadFontResource("Norm Regular", Norm_Regular_ttf);
-  loadFontResource("Outward Block", Outward_Block_ttf);
-  loadFontResource("Piazzolla Medium", Piazzolla_Medium_ttf);
-  loadFontResource("SnapitMono Regular", SnapitMono_Regular_ttf);
-  loadFontResource("SpaceGrotesk Regular", SpaceGrotesk_Regular_ttf);
-  loadFontResource("StandardGraf Regular", StandardGraf_Regular_ttf);
-  loadFontResource("Syne Extra", Syne_Extra_ttf);
-  loadFontResource("YatraOne Regular", YatraOne_Regular_ttf);
-  loadFontResource("Zarathustra Regular", Zarathustra_Regular_ttf);
+  loadFontResource("Twemoji");
+  loadFontResource("OpenSansEmoji");
+  loadFontResource("3270Condensed-Condensed");
+  loadFontResource("Abibas-Medium");
+  loadFontResource("AstralMono-Regular");
+  loadFontResource("Avara-Bold");
+  loadFontResource("Avara-BoldItalic");
+  loadFontResource("Betatron-Regular");
+  loadFontResource("Blocus-Regular");
+  loadFontResource("BreiteGrotesk-Regular");
+  loadFontResource("Chicagoland-Medium");
+  loadFontResource("ComicNeue-Bold");
+  loadFontResource("ComicNeueAngular-Bold");
+  loadFontResource("Compagnon-Bold");
+  loadFontResource("Compagnon-Medium");
+  loadFontResource("Compagnon-Roman");
+  loadFontResource("DagsenOutline-Black");
+  loadFontResource("Glacier-Bold");
+  loadFontResource("HappyTimesAtTheIKOB-Regular");
+  loadFontResource("HelicoCentrica-Roman");
+  loadFontResource("Norm-Medium");
+  loadFontResource("Norm-Regular");
+  loadFontResource("Outward-Block");
+  loadFontResource("Piazzolla-Medium");
+  loadFontResource("SnapitMono-Regular");
+  loadFontResource("SpaceGrotesk-Regular");
+  loadFontResource("StandardGraf-Regular");
+  loadFontResource("Syne-Extra");
+  loadFontResource("YatraOne-Regular");
+  loadFontResource("Zarathustra-Regular");
 }
