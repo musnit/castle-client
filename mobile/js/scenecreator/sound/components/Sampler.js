7a1fa652b8d83ecf63bd0a4dfb5ac3f8e7196c8b
Ben Roth <ben@castle.xyz>
2022-01-06 15:15:03 -0800

    scene creator: sound: allow choosing sampler sample type

--- mobile/js/scenecreator/sound/components/Sampler.js
+++ mobile/js/scenecreator/sound/components/Sampler.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { InspectorDropdown } from '../../inspector/components/InspectorDropdown';
 import { SAMPLE_COMPONENTS } from './Sample';
 import { sendAsync } from '../../../core/CoreEvents';
 
+import Metadata from '../../Metadata';
+
+import * as Constants from '../../../Constants';
+
 const styles = StyleSheet.create({
   container: {
-    paddingHorizontal: 16,
+    padding: 16,
+    maxWidth: Constants.TABLET_MAX_FORM_WIDTH,
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  label: {
+    fontSize: 16,
+    color: '#000',
+    paddingRight: 12,
   },
 });
 
@@ -21,11 +36,23 @@ export const Sampler = ({ instrument }) => {
     },
     [instrument]
   );
+  const onChangeType = React.useCallback(
+    (type) => onChangeSample({ ...instrument.sample, type }),
+    [onChangeSample]
+  );
   if (instrument?.sample) {
     const SampleComponent = SAMPLE_COMPONENTS[instrument.sample.type];
     if (SampleComponent) {
       return (
         <View style={styles.container}>
+          <View style={styles.row}>
+            <Text style={styles.label}>Sample type:</Text>
+            <InspectorDropdown
+              value={instrument.sample.type}
+              onChange={onChangeType}
+              {...Metadata.responses['play sound'].props.type}
+            />
+          </View>
           <SampleComponent params={instrument.sample} onChangeParams={onChangeSample} />
         </View>
       );
