e46598f1d0e72ae24c9e7c0638dffeae11108440
Nikhilesh S <s.nikhilesh@gmail.com>
2022-01-11 23:59:25 -0800

    mobile: make native feed pass overlay text font style

--- mobile/js/components/NativeDecksFeed.js
+++ mobile/js/components/NativeDecksFeed.js
@@ -1,11 +1,15 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { GameView } from '../game/GameView';
+import * as Constants from '../Constants';
 
 export const NativeDecksFeed = () => {
-  return <GameView
-    initialParams={JSON.stringify({
-      useNativeFeed: true,
-    })}
-    paused={false}
-  />
+  return (
+    <GameView
+      initialParams={JSON.stringify({
+        useNativeFeed: true,
+        textOverlayStyle: Constants.CORE_OVERLAY_TEXT_STYLE,
+      })}
+      paused={false}
+    />
+  );
 };
