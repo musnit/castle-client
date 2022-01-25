1a2d6a8086fdffec993c5d914fe8e126dddc212b
David Cole <david@castle.xyz>
2022-01-25 14:47:13 -0800

    text: add link to font licenses in app

--- mobile/js/profile/MiscLinks.js
+++ mobile/js/profile/MiscLinks.js
@@ -24,8 +24,8 @@ export const MiscLinks = () => {
           <Text style={styles.link}>Log out</Text>
         </TouchableOpacity>
       ) : null}
-      <TouchableOpacity onPress={() => Linking.openURL(Constants.DISCORD_INVITE_LINK)}>
-        <Text style={styles.link}>Discord</Text>
+      <TouchableOpacity onPress={() => Linking.openURL('https://castle.xyz/colophon')}>
+        <Text style={styles.link}>Colophon</Text>
       </TouchableOpacity>
       <TouchableOpacity onPress={() => Linking.openURL('https://castle.xyz/terms')}>
         <Text style={styles.link}>Terms</Text>
