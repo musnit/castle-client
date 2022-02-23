4d44903fc35edf9010e8927dd5f0d05b6acbc702
Ben Roth <ben@castle.xyz>
2022-02-23 10:51:48 -0800

    music: clean up drums component

--- mobile/js/scenecreator/sound/components/Drums.js
+++ mobile/js/scenecreator/sound/components/Drums.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import { InspectorCheckbox } from '../../inspector/components/InspectorCheckbox';
 import { InspectorDropdown } from '../../inspector/components/InspectorDropdown';
-import { InspectorKnob } from '../../inspector/components/InspectorKnob';
 import { InspectorNumberInput } from '../../inspector/components/InspectorNumberInput';
 import { SAMPLE_COMPONENTS } from './Sample';
 import { sendAsync } from '../../../core/CoreEvents';
@@ -14,15 +13,18 @@ import * as Constants from '../../../Constants';
 const styles = StyleSheet.create({
   container: {
     padding: 16,
+    flexDirection: 'row',
   },
   drumContainer: {
+    flex: 1,
+    minWidth: 256,
     backgroundColor: '#fff',
     ...Constants.styles.dropShadow,
     padding: 16,
     borderWidth: 1,
     borderRadius: 6,
     borderColor: Constants.colors.black,
-    marginBottom: 8,
+    marginRight: 16,
   },
   drumParams: {
     marginTop: 16,
@@ -55,127 +57,124 @@ const styles = StyleSheet.create({
   },
 });
 
-const Kick = ({ value, onChange, enabled, onSetEnabled, lastNativeUpdate }) => {
-  const onChangeFreq = React.useCallback(
-    (freq) =>
-      onChange({
-        ...value,
-        freq,
-      }),
-    [value, onChange]
-  );
-  const onChangeDecay = React.useCallback(
-    (decay) =>
-      onChange({
-        ...value,
-        decay,
-      }),
-    [value, onChange]
-  );
-  const onChangePunch = React.useCallback(
-    (punch) =>
-      onChange({
-        ...value,
-        punch,
-      }),
-    [value, onChange]
-  );
-  const onChangeSweep = React.useCallback(
-    (sweep) =>
-      onChange({
-        ...value,
-        sweep,
-      }),
-    [value, onChange]
-  );
-
-  return (
-    <View style={styles.drumContainer}>
-      <View style={styles.row}>
-        <Text style={styles.label}>Kick</Text>
-        <InspectorCheckbox value={enabled} onChange={onSetEnabled} />
-      </View>
-      {enabled ? (
-        <View style={styles.drumParams}>
-          <View style={styles.soundInputsRow}>
-            <Text style={styles.soundInputLabel}>Decay</Text>
-            <InspectorKnob
-              style={styles.soundInput}
-              lastNativeUpdate={lastNativeUpdate}
-              min={0.05}
-              max={1}
-              step={0.05}
-              placeholder="Decay"
-              value={value.decay}
-              onChange={onChangeDecay}
-            />
-          </View>
-          <View style={styles.soundInputsRow}>
-            <Text style={styles.soundInputLabel}>Punch</Text>
-            <InspectorNumberInput
-              style={styles.soundInput}
-              lastNativeUpdate={lastNativeUpdate}
-              min={0}
-              max={1}
-              step={0.1}
-              placeholder="Punch"
-              value={value.punch}
-              onChange={onChangePunch}
-            />
-          </View>
-          <View style={styles.soundInputsRow}>
-            <Text style={styles.soundInputLabel}>Tune</Text>
-            <InspectorNumberInput
-              style={styles.soundInput}
-              lastNativeUpdate={lastNativeUpdate}
-              min={40}
-              max={120}
-              placeholder="Tune"
-              value={value.freq}
-              onChange={onChangeFreq}
-            />
-          </View>
-          <View style={styles.soundInputsRow}>
-            <Text style={styles.soundInputLabel}>Sweep</Text>
-            <InspectorNumberInput
-              style={styles.soundInput}
-              lastNativeUpdate={lastNativeUpdate}
-              min={0}
-              max={1}
-              step={0.1}
-              placeholder="Sweep"
-              value={value.sweep}
-              onChange={onChangeSweep}
-            />
-          </View>
-        </View>
-      ) : null}
-    </View>
-  );
+const DRUM_PARAMS = {
+  kick: [
+    {
+      name: 'decay',
+      label: 'Decay',
+      min: 0.05,
+      max: 1,
+      step: 0.05,
+    },
+    {
+      name: 'punch',
+      label: 'Punch',
+      min: 0,
+      max: 1,
+      step: 0.1,
+    },
+    {
+      name: 'freq',
+      label: 'Tune',
+      min: 40,
+      max: 120,
+    },
+    {
+      name: 'sweep',
+      label: 'Sweep',
+      min: 0,
+      max: 1,
+      step: 0.1,
+    },
+  ],
+  snare: [
+    {
+      name: 'decay',
+      label: 'Decay',
+      min: 0.1,
+      max: 0.5,
+      step: 0.05,
+    },
+    {
+      name: 'freq',
+      label: 'Tone',
+      min: 0,
+      max: 1,
+      step: 0.1,
+    },
+    {
+      name: 'tambre',
+      label: 'Tambre',
+      min: 0,
+      max: 1,
+      step: 0.2,
+    },
+  ],
+  clap: [
+    {
+      name: 'decay',
+      label: 'Decay',
+      min: 0,
+      max: 0.5,
+      step: 0.05,
+    },
+    {
+      name: 'freq',
+      label: 'Tone',
+      min: 0,
+      max: 1,
+      step: 0.1,
+    },
+  ],
+  tom: [
+    {
+      name: 'decay',
+      label: 'Decay',
+      min: 0.1,
+      max: 0.4,
+      step: 0.05,
+    },
+    {
+      name: 'freq',
+      label: 'Tone',
+      min: 0,
+      max: 1,
+      step: 0.1,
+    },
+  ],
+  hat: [
+    {
+      name: 'decay',
+      label: 'Decay',
+      min: 0.05,
+      max: 0.35,
+      step: 0.05,
+    },
+    {
+      name: 'freq',
+      label: 'Tone',
+      min: 0.5,
+      max: 1,
+      step: 0.1,
+    },
+    {
+      name: 'body',
+      label: 'Body',
+      min: 0,
+      max: 1,
+      step: 0.2,
+    },
+  ],
 };
 
-const Hat = ({ value, onChange, enabled, onSetEnabled, lastNativeUpdate }) => {
-  const onChangeFreq = React.useCallback(
-    (freq) =>
+const Drum = ({ name, value, onChange, enabled, onSetEnabled, paramSpecs, lastNativeUpdate }) => {
+  // paramSpecs: array
+  //   --> { name, min, max, step }
+  const onChangeParam = React.useCallback(
+    (paramName, paramVal) =>
       onChange({
         ...value,
-        freq,
-      }),
-    [value, onChange]
-  );
-  const onChangeDecay = React.useCallback(
-    (decay) =>
-      onChange({
-        ...value,
-        decay,
-      }),
-    [value, onChange]
-  );
-  const onChangeBody = React.useCallback(
-    (body) =>
-      onChange({
-        ...value,
-        body,
+        [paramName]: paramVal,
       }),
     [value, onChange]
   );
@@ -183,245 +182,24 @@ const Hat = ({ value, onChange, enabled, onSetEnabled, lastNativeUpdate }) => {
   return (
     <View style={styles.drumContainer}>
       <View style={styles.row}>
-        <Text style={styles.label}>Hat</Text>
+        <Text style={styles.label}>{name}</Text>
         <InspectorCheckbox value={enabled} onChange={onSetEnabled} />
       </View>
       {enabled ? (
         <View style={styles.drumParams}>
-          <View style={styles.soundInputsRow}>
-            <Text style={styles.soundInputLabel}>Decay</Text>
-            <InspectorNumberInput
-              style={styles.soundInput}
-              lastNativeUpdate={lastNativeUpdate}
-              min={0.05}
-              max={0.35}
-              step={0.05}
-              placeholder="Decay"
-              value={value.decay}
-              onChange={onChangeDecay}
-            />
-          </View>
-          <View style={styles.soundInputsRow}>
-            <Text style={styles.soundInputLabel}>Tone</Text>
-            <InspectorNumberInput
-              style={styles.soundInput}
-              lastNativeUpdate={lastNativeUpdate}
-              min={0.5}
-              max={1}
-              step={0.1}
-              placeholder="Tone"
-              value={value.freq}
-              onChange={onChangeFreq}
-            />
-          </View>
-          <View style={styles.soundInputsRow}>
-            <Text style={styles.soundInputLabel}>Body</Text>
-            <InspectorNumberInput
-              style={styles.soundInput}
-              lastNativeUpdate={lastNativeUpdate}
-              min={0}
-              max={1}
-              step={0.2}
-              placeholder="Body"
-              value={value.body}
-              onChange={onChangeBody}
-            />
-          </View>
-        </View>
-      ) : null}
-    </View>
-  );
-};
-
-const Snare = ({ value, onChange, enabled, onSetEnabled, lastNativeUpdate }) => {
-  const onChangeFreq = React.useCallback(
-    (freq) =>
-      onChange({
-        ...value,
-        freq,
-      }),
-    [value, onChange]
-  );
-  const onChangeDecay = React.useCallback(
-    (decay) =>
-      onChange({
-        ...value,
-        decay,
-      }),
-    [value, onChange]
-  );
-  const onChangeTambre = React.useCallback(
-    (tambre) =>
-      onChange({
-        ...value,
-        tambre,
-      }),
-    [value, onChange]
-  );
-
-  return (
-    <View style={styles.drumContainer}>
-      <View style={styles.row}>
-        <Text style={styles.label}>Snare</Text>
-        <InspectorCheckbox value={enabled} onChange={onSetEnabled} />
-      </View>
-      {enabled ? (
-        <View style={styles.drumParams}>
-          <View style={styles.soundInputsRow}>
-            <Text style={styles.soundInputLabel}>Decay</Text>
-            <InspectorNumberInput
-              style={styles.soundInput}
-              lastNativeUpdate={lastNativeUpdate}
-              min={0.1}
-              max={0.5}
-              step={0.05}
-              placeholder="Decay"
-              value={value.decay}
-              onChange={onChangeDecay}
-            />
-          </View>
-          <View style={styles.soundInputsRow}>
-            <Text style={styles.soundInputLabel}>Tone</Text>
-            <InspectorNumberInput
-              style={styles.soundInput}
-              lastNativeUpdate={lastNativeUpdate}
-              min={0}
-              max={1}
-              step={0.1}
-              placeholder="Tone"
-              value={value.freq}
-              onChange={onChangeFreq}
-            />
-          </View>
-          <View style={styles.soundInputsRow}>
-            <Text style={styles.soundInputLabel}>Tambre</Text>
-            <InspectorNumberInput
-              style={styles.soundInput}
-              lastNativeUpdate={lastNativeUpdate}
-              min={0}
-              max={1}
-              step={0.2}
-              placeholder="Tambre"
-              value={value.tambre}
-              onChange={onChangeTambre}
-            />
-          </View>
-        </View>
-      ) : null}
-    </View>
-  );
-};
-
-const Clap = ({ value, onChange, enabled, onSetEnabled, lastNativeUpdate }) => {
-  const onChangeFreq = React.useCallback(
-    (freq) =>
-      onChange({
-        ...value,
-        freq,
-      }),
-    [value, onChange]
-  );
-  const onChangeDecay = React.useCallback(
-    (decay) =>
-      onChange({
-        ...value,
-        decay,
-      }),
-    [value, onChange]
-  );
-
-  return (
-    <View style={styles.drumContainer}>
-      <View style={styles.row}>
-        <Text style={styles.label}>Clap</Text>
-        <InspectorCheckbox value={enabled} onChange={onSetEnabled} />
-      </View>
-      {enabled ? (
-        <View style={styles.drumParams}>
-          <View style={styles.soundInputsRow}>
-            <Text style={styles.soundInputLabel}>Decay</Text>
-            <InspectorNumberInput
-              style={styles.soundInput}
-              lastNativeUpdate={lastNativeUpdate}
-              min={0}
-              max={0.5}
-              step={0.05}
-              placeholder="Decay"
-              value={value.decay}
-              onChange={onChangeDecay}
-            />
-          </View>
-          <View style={styles.soundInputsRow}>
-            <Text style={styles.soundInputLabel}>Tone</Text>
-            <InspectorNumberInput
-              style={styles.soundInput}
-              lastNativeUpdate={lastNativeUpdate}
-              min={0}
-              max={1}
-              step={0.1}
-              placeholder="Tone"
-              value={value.freq}
-              onChange={onChangeFreq}
-            />
-          </View>
-        </View>
-      ) : null}
-    </View>
-  );
-};
-
-const Tom = ({ value, onChange, enabled, onSetEnabled, lastNativeUpdate }) => {
-  const onChangeFreq = React.useCallback(
-    (freq) =>
-      onChange({
-        ...value,
-        freq,
-      }),
-    [value, onChange]
-  );
-  const onChangeDecay = React.useCallback(
-    (decay) =>
-      onChange({
-        ...value,
-        decay,
-      }),
-    [value, onChange]
-  );
-
-  return (
-    <View style={styles.drumContainer}>
-      <View style={styles.row}>
-        <Text style={styles.label}>Tom</Text>
-        <InspectorCheckbox value={enabled} onChange={onSetEnabled} />
-      </View>
-      {enabled ? (
-        <View style={styles.drumParams}>
-          <View style={styles.soundInputsRow}>
-            <Text style={styles.soundInputLabel}>Decay</Text>
-            <InspectorNumberInput
-              style={styles.soundInput}
-              lastNativeUpdate={lastNativeUpdate}
-              min={0.1}
-              max={0.4}
-              step={0.05}
-              placeholder="Decay"
-              value={value.decay}
-              onChange={onChangeDecay}
-            />
-          </View>
-          <View style={styles.soundInputsRow}>
-            <Text style={styles.soundInputLabel}>Tone</Text>
-            <InspectorNumberInput
-              style={styles.soundInput}
-              lastNativeUpdate={lastNativeUpdate}
-              min={0}
-              max={1}
-              step={0.1}
-              placeholder="Tone"
-              value={value.freq}
-              onChange={onChangeFreq}
-            />
-          </View>
+          {paramSpecs.map((spec) => (
+            <View style={styles.soundInputsRow} key={`spec-${spec.name}`}>
+              <Text style={styles.soundInputLabel}>{spec.label}</Text>
+              <InspectorNumberInput
+                style={styles.soundInput}
+                lastNativeUpdate={lastNativeUpdate}
+                {...spec}
+                placeholder={spec.label}
+                value={value[spec.name]}
+                onChange={(paramVal) => onChangeParam(spec.name, paramVal)}
+              />
+            </View>
+          ))}
         </View>
       ) : null}
     </View>
@@ -453,50 +231,64 @@ export const Drums = ({ instrument }) => {
 
   if (instrument?.params) {
     return (
-      <ScrollView style={styles.container} horizontal>
-        <Kick
+      <ScrollView style={{ flex: 1 }} contentContainerStyle={styles.container} horizontal>
+        <Drum
+          name="Kick"
+          paramSpecs={DRUM_PARAMS.kick}
           enabled={instrument.params.useKick}
           onSetEnabled={(enabled) => onChangeDrum('useKick', enabled)}
           value={instrument.params.kick}
           onChange={(kick) => onChangeDrum('kick', kick)}
           lastNativeUpdate={lastNativeUpdate}
         />
-        <Tom
+        <Drum
+          name="Low Tom"
+          paramSpecs={DRUM_PARAMS.tom}
           enabled={instrument.params.useLoTom}
           onSetEnabled={(enabled) => onChangeDrum('useLoTom', enabled)}
           value={instrument.params.loTom}
           onChange={(loTom) => onChangeDrum('loTom', loTom)}
           lastNativeUpdate={lastNativeUpdate}
         />
-        <Tom
+        <Drum
+          name="High Tom"
+          paramSpecs={DRUM_PARAMS.tom}
           enabled={instrument.params.useHiTom}
           onSetEnabled={(enabled) => onChangeDrum('useHiTom', enabled)}
           value={instrument.params.hiTom}
           onChange={(hiTom) => onChangeDrum('hiTom', hiTom)}
           lastNativeUpdate={lastNativeUpdate}
         />
-        <Snare
+        <Drum
+          name="Snare"
+          paramSpecs={DRUM_PARAMS.snare}
           enabled={instrument.params.useSnare}
           onSetEnabled={(enabled) => onChangeDrum('useSnare', enabled)}
           value={instrument.params.snare}
           onChange={(snare) => onChangeDrum('snare', snare)}
           lastNativeUpdate={lastNativeUpdate}
         />
-        <Clap
+        <Drum
+          name="Clap"
+          paramSpecs={DRUM_PARAMS.clap}
           enabled={instrument.params.useClap}
           onSetEnabled={(enabled) => onChangeDrum('useClap', enabled)}
           value={instrument.params.clap}
           onChange={(clap) => onChangeDrum('clap', clap)}
           lastNativeUpdate={lastNativeUpdate}
         />
-        <Hat
+        <Drum
+          name="Closed Hat"
+          paramSpecs={DRUM_PARAMS.hat}
           enabled={instrument.params.useClosedHat}
           onSetEnabled={(enabled) => onChangeDrum('useClosedHat', enabled)}
           value={instrument.params.closedHat}
           onChange={(closedHat) => onChangeDrum('closedHat', closedHat)}
           lastNativeUpdate={lastNativeUpdate}
         />
-        <Hat
+        <Drum
+          name="Open Hat"
+          paramSpecs={DRUM_PARAMS.hat}
           enabled={instrument.params.useOpenHat}
           onSetEnabled={(enabled) => onChangeDrum('useOpenHat', enabled)}
           value={instrument.params.openHat}
