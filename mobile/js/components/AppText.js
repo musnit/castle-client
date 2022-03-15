b9d13892f71030e333e2ec124b5cfcafc07c08ef
Ben Roth <ben@castle.xyz>
2022-03-15 10:25:33 -0700

    js: add `AppText` with custom text styles

---
+++ mobile/js/components/AppText.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text } from 'react-native';
+
+const base = StyleSheet.create({
+  text: {
+    fontFamily: 'Helvetica',
+  },
+});
+
+export const AppText = ({ style, children, ...props }) => {
+  let styles;
+  if (Array.isArray(style)) {
+    styles = [base.text, ...style];
+  } else {
+    styles = [base.text, style];
+  }
+  return (
+    <Text {...props} style={styles}>
+      {children}
+    </Text>
+  );
+};
