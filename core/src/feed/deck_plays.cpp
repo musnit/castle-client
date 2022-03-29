73b42ad68135a6164148356c3b650865e17b0278
Jesse Ruder <jesseruder@gmail.com>
2022-03-29 12:39:57 -0700

    core: update deck plays

--- core/src/feed/deck_plays.cpp
+++ core/src/feed/deck_plays.cpp
@@ -1,7 +1,6 @@
 #include "deck_plays.h"
 #include "api.h"
 
-const double INITIAL_UPDATE_INTERVAL = 1.0;
 const double UPDATE_INTERVAL = 3.0;
 
 DeckPlays::DeckPlays() {
@@ -10,11 +9,10 @@ DeckPlays::DeckPlays() {
 
 void DeckPlays::setDeckAndCardIds(std::string deckId_, std::string cardId_) {
   if (deckId != deckId_ || cardId != cardId_) {
-    sendUpdate();
-    // Don't send next update immediately because we don't want server to get them out of order
-    timeSinceLastUpdate = UPDATE_INTERVAL - INITIAL_UPDATE_INTERVAL;
+    timeSinceLastUpdate = 0.0;
     deckId = deckId_;
     cardId = cardId_;
+    sendUpdate();
   }
 }
 
