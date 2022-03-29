3d42d13f75040421c1e970e96c7b6326f179b4b0
Ben Roth <ben@castle.xyz>
2022-03-29 10:50:20 -0700

    comments sheet: add parent and child remix attribution

--- mobile/js/comments/CommentsSheetHeader.js
+++ mobile/js/comments/CommentsSheetHeader.js
@@ -1,23 +1,30 @@
 import * as React from 'react';
 import { Pressable, StyleSheet, TouchableOpacity, View } from 'react-native';
 import { AppText as Text } from '../components/AppText';
+import { formatCount } from '../common/utilities';
+import { gql } from '@apollo/client';
 import { useNavigation } from '../ReactNavigation';
 import { UserAvatar } from '../components/UserAvatar';
 
 import * as Constants from '../Constants';
+import * as Session from '../Session';
 import * as SceneCreatorConstants from '../scenecreator/SceneCreatorConstants';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const CastleIcon = Constants.CastleIcon;
 const ICON_SIZE = 34;
 
 const styles = StyleSheet.create({
   header: {
-    flexDirection: 'row',
     borderBottomWidth: 1,
     borderBottomColor: '#ccc',
-    justifyContent: 'space-between',
+    paddingTop: 16,
+  },
+  row: {
     alignItems: 'center',
-    paddingVertical: 16,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   headerLeft: {
     flexDirection: 'row',
@@ -40,6 +47,43 @@ const styles = StyleSheet.create({
     fontSize: 16,
     marginLeft: 10,
   },
+  parentAttrib: {
+    flexDirection: 'row',
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 3,
+    padding: 4,
+    marginTop: 8,
+    alignItems: 'center',
+  },
+  parentUsername: {
+    paddingLeft: 4,
+    color: '#888',
+    fontSize: 16,
+  },
+  commentsHeading: {
+    borderBottomWidth: 2,
+    borderBottomColor: '#000',
+    paddingBottom: 8,
+    paddingTop: 16,
+  },
+  commentsLabel: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    textTransform: 'uppercase',
+  },
+  childAttrib: {
+    paddingTop: 16,
+    paddingBottom: 8,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  childLabel: {
+    fontSize: 16,
+    textTransform: 'uppercase',
+    color: '#888',
+    paddingRight: 4,
+  },
   back: {
     flexShrink: 0,
     width: 60,
@@ -49,24 +93,85 @@ const styles = StyleSheet.create({
   },
 });
 
-export const CommentsSheetHeader = ({ deck, onClose }) => {
+export const CommentsSheetHeader = ({ deck = {}, onClose }) => {
   const { push, navigate } = useNavigation();
+
+  const onPressRemix = React.useCallback(() => {
+    return push('DeckRemixes', {
+      deck,
+      isFullscreen: true,
+    });
+  }, [deck]);
+
+  const navigateToParent = React.useCallback(async () => {
+    const result = await Session.apolloClient.query({
+      query: gql`
+          query GetDeckById($deckId: ID!) {
+            deck(deckId: $deckId) {
+              ${Constants.FEED_ITEM_DECK_FRAGMENT}
+            }
+          }
+        `,
+      variables: { deckId: deck.parentDeckId },
+    });
+    if (result?.data?.deck && result.data.deck.visibility === 'public') {
+      return navigate(
+        'DeckRemixes',
+        {
+          deck: result.data.deck,
+        },
+        {
+          isFullscreen: true,
+        }
+      );
+    }
+  }, [deck?.parentDeckId, navigate]);
+
+  if (!deck?.creator) {
+    return null;
+  }
   const { creator } = deck;
+
   return (
     <View style={styles.header}>
-      <View style={styles.headerLeft} pointerEvents="box-none">
-        <Pressable
-          style={styles.creator}
-          onPress={() => push('Profile', { userId: creator.userId })}>
-          <View style={styles.avatar}>
-            <UserAvatar url={creator.photo?.url} />
-          </View>
-          <Text style={styles.username}>{creator.username}</Text>
-        </Pressable>
+      <View style={styles.row}>
+        <View style={styles.headerLeft} pointerEvents="box-none">
+          <Pressable
+            style={styles.creator}
+            onPress={() => push('Profile', { userId: creator.userId })}>
+            <View style={styles.avatar}>
+              <UserAvatar url={creator.photo?.url} />
+            </View>
+            <Text style={styles.username}>{creator.username}</Text>
+          </Pressable>
+        </View>
+        <TouchableOpacity style={styles.back} onPress={onClose}>
+          <CastleIcon name="close" size={22} color="#000" />
+        </TouchableOpacity>
       </View>
-      <TouchableOpacity style={styles.back} onPress={onClose}>
-        <CastleIcon name="close" size={22} color="#000" />
-      </TouchableOpacity>
+      {deck.parentDeckId && deck.parentDeck ? (
+        <View style={[styles.row, { paddingHorizontal: 16 }]}>
+          <View style={styles.parentAttrib}>
+            <CastleIcon name="remix" color="#888" size={12} style={styles.remixIcon} />
+            <Pressable onPress={navigateToParent}>
+              <Text numberOfLines={1} style={styles.parentUsername}>
+                Remixed {deck.parentDeck?.creator?.username}
+              </Text>
+            </Pressable>
+          </View>
+        </View>
+      ) : null}
+      <View style={[styles.row, { paddingHorizontal: 16 }]}>
+        <View style={styles.commentsHeading}>
+          <Text style={styles.commentsLabel}>{formatCount(deck.comments.count)} comments</Text>
+        </View>
+        {deck.childDecksCount > 0 ? (
+          <Pressable style={styles.childAttrib} onPress={onPressRemix}>
+            <Text style={styles.childLabel}>View {formatCount(deck.childDecksCount)} remixes</Text>
+            <Icon name="arrow-forward" color="#888" size={16} />
+          </Pressable>
+        ) : null}
+      </View>
     </View>
   );
 };
--- mobile/js/common/utilities.js
+++ mobile/js/common/utilities.js
@@ -300,3 +300,22 @@ export const preloadImageRequires = (images) => {
 
   FastImage.preload(uris);
 };
+
+// https://stackoverflow.com/questions/9461621/format-a-number-as-2-5k-if-a-thousand-or-more-otherwise-900
+const lookup = [
+  { value: 1, symbol: '' },
+  { value: 1e3, symbol: 'k' },
+  { value: 1e6, symbol: 'M' },
+  { value: 1e9, symbol: 'G' },
+  { value: 1e12, symbol: 'T' },
+  { value: 1e15, symbol: 'P' },
+  { value: 1e18, symbol: 'E' },
+];
+export const formatCount = (num, digits = 1) => {
+  const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+  var item = lookup
+    .slice()
+    .reverse()
+    .find((item) => num >= item.value);
+  return item ? (num / item.value).toFixed(digits).replace(rx, '$1') + item.symbol : '0';
+};
