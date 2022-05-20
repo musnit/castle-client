f743700b16e3e9044dbb118757530216f662c743
Jesse Ruder <jesseruder@gmail.com>
2022-05-20 13:48:49 -0700

    android: update ci script

--- mobile/tools/build-android-prod-ci.sh
+++ mobile/tools/build-android-prod-ci.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -e
 
+pushd .. > /dev/null
+
+git clone https://$GHOST_SECRET_GITHUB_TOKEN@github.com/castle-xyz/ghost-server.git
+
+popd > /dev/null
+
 DIRNAME=`dirname "$0"`
 pushd $DIRNAME > /dev/null
 
@@ -9,21 +15,9 @@ mkdir build
 
 pushd .. > /dev/null
 
-echo ""
-echo "Writing .env file"
-if [ ! -f .env ]
-then
-printf "AMPLITUDE_KEY=\"$AMPLITUDE_KEY\"\nSENTRY_DSN=\"$SENTRY_DSN\"\nADJUST_APP_TOKEN=\"$ADJUST_APP_TOKEN\"" > .env
-fi
-
-echo ""
-echo "Writing CastleSecret.java file"
-
-mkdir -p android/app/src/main/java/xyz/castle/generated/
-if [ ! -f android/app/src/main/java/xyz/castle/generated/CastleSecret.java ]
-then
-printf "package xyz.castle.generated;\n\npublic final class CastleSecret {\n  public static final String ADJUST_APP_TOKEN = \"$ADJUST_APP_TOKEN\";\n}\n" > android/app/src/main/java/xyz/castle/generated/CastleSecret.java
-fi
+cp ../../ghost-secret/mobile/.env .env
+cp ../../ghost-secret/android-release/my-upload-key.keystore android/app/my-upload-key.keystore
+cp ../../ghost-secret/android-release/gradle.properties android/gradle.properties
 
 popd > /dev/null
 
